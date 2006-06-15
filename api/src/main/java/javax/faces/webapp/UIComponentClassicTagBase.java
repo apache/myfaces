16a3ba888c45ee46a863e5c76681ae77c714d948
Dennis Christopher Byrne <dennisbyrne@apache.org>
2006-06-15 07:38:21 +0000

    implemented 8 methods

--- api/src/main/java/javax/faces/webapp/UIComponentClassicTagBase.java
+++ api/src/main/java/javax/faces/webapp/UIComponentClassicTagBase.java
@@ -1,8 +1,10 @@
 package javax.faces.webapp;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.Application;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIOutput;
 import javax.faces.component.UIViewRoot;
@@ -41,6 +43,14 @@ public abstract class UIComponentClassicTagBase extends UIComponentTagBase
     
     private String jspId;
     
+    private List<String> children ;
+    
+    private List<String> facets ;
+    
+    private boolean created ;
+    
+    private static final String VERBATIM_COMP_TYPE = "javax.faces.HtmlOutputText";
+    
     public void doInitBody() throws JspException
     {
         throw new UnsupportedOperationException("1.2");
@@ -56,48 +66,7 @@ public abstract class UIComponentClassicTagBase extends UIComponentTagBase
         throw new UnsupportedOperationException("1.2");
     }
 
-    private boolean isDuplicateId(String componentId)
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    private String generateIncrementedId(String componentId)
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    protected List<String> getCreatedComponents()
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    private String createId() throws JspException
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
     public void setJspId(String id)
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    private void updatePreviousJspIdAndIteratorStatus(String id)
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    private boolean isIncludedOrForwarded()
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-    
-    private UIComponent createFacet(FacesContext context, UIComponent parent,
-            String name, String newId) throws JspException
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    private UIComponent getChild(UIComponent component, String componentId)
     {
         throw new UnsupportedOperationException("1.2");
     }
@@ -110,67 +79,6 @@ public abstract class UIComponentClassicTagBase extends UIComponentTagBase
 
     public static UIComponentClassicTagBase getParentUIComponentClassicTagBase(
             PageContext context)
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    protected int getIndexOfNextChildTag()
-    {
-
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    protected void addChild(UIComponent child)
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    protected void addFacet(String name)
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    private void popUIComponentClassicTagBase()
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    private void pushUIComponentClassicTagBase()
-    {
-        throw new UnsupportedOperationException("1.2");
-
-    }
-
-    private void removeOldChildren()
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    private void removeOldFacets()
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    protected UIComponent createVerbatimComponentFromBodyContent()
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    protected UIOutput createVerbatimComponent()
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    protected void addVerbatimBeforeComponent(
-            UIComponentClassicTagBase parentTag, UIComponent verbatim,
-            UIComponent component)
-    {
-        throw new UnsupportedOperationException("1.2");
-    }
-
-    protected void addVerbatimAfterComponent(
-            UIComponentClassicTagBase parentTag, UIComponent verbatim,
-            UIComponent component)
     {
         throw new UnsupportedOperationException("1.2");
     }
@@ -195,33 +103,160 @@ public abstract class UIComponentClassicTagBase extends UIComponentTagBase
         throw new UnsupportedOperationException("1.2");
     }
 
-    public boolean getCreated()
+    protected UIComponent createVerbatimComponentFromBodyContent()
     {
-        throw new UnsupportedOperationException("1.2");
+        
+        // the 'single exit point' rule made this look like perl
+        
+        if( bodyContent == null)
+            return null;
+        
+        String bodyString = bodyContent.getString();
+        
+        if( bodyString == null) // nothing
+            return null;
+        
+        bodyContent.clearBody();
+        
+        String trimmedBody = bodyString.trim();
+        
+        if( "".equals(trimmedBody) ) // whitespace
+            return null;
+        
+        if( trimmedBody.startsWith("<!--") 
+                && trimmedBody.endsWith("-->") ) // comments
+            return null;
+        
+        UIOutput output = createVerbatimComponent();
+        output.setValue(bodyString);
+        
+        return output;
     }
-
+    
+    /**
+     * Helper method for addVerbatimBeforeComponent and addVerbatimAfterComponent.
+     */
+    
+    private void addVerbatimComponent(
+            UIComponentClassicTagBase parentTag, 
+            UIComponent verbatim,
+            UIComponent component, int shift){
+        
+        if( component == null )
+            throw new NullPointerException("UIComponent");
+        
+        UIComponent parent = component.getParent();
+        
+        if( parent != null ){
+            
+            List<UIComponent> brood = parent.getChildren();
+            
+            if( ! brood.contains(component) )
+                throw new IllegalStateException("Child component can find parent, but parent does not claim child ");
+            
+            brood.add(brood.indexOf(component) + shift, verbatim);
+            
+        }else if( parent == null && ! ( component instanceof UIViewRoot ) )
+            throw new NullPointerException("Could not find parent ");
+        
+    }
+    
+    protected void addVerbatimAfterComponent(
+            UIComponentClassicTagBase parentTag, UIComponent verbatim,
+            UIComponent component)
+    {
+        addVerbatimComponent(parentTag, verbatim, component, 1);
+    }
+    
+    protected void addVerbatimBeforeComponent(
+            UIComponentClassicTagBase parentTag, UIComponent verbatim,
+            UIComponent component)
+    {
+        addVerbatimComponent(parentTag, verbatim, component, 0);
+    }
+    
+    protected UIOutput createVerbatimComponent()
+    {
+        FacesContext _ctx = getFacesContext();
+        
+        if( _ctx == null )
+            throw new NullPointerException("FacesContext");
+        
+        Application app = _ctx.getApplication();
+        
+        if( app == null )
+            throw new NullPointerException("Application");
+        
+        UIViewRoot root = _ctx.getViewRoot();
+        
+        if( root == null )
+            throw new NullPointerException("UIViewRoot");    
+        
+        UIComponent component = app.createComponent(VERBATIM_COMP_TYPE);
+        component.setTransient(false);
+        component.getAttributes().put("escape", Boolean.FALSE);
+        component.setId(root.createUniqueId());
+        
+        return (UIOutput)component;
+    }
+    
     protected FacesContext getFacesContext()
     {
-        throw new UnsupportedOperationException("1.2");
+        return ctx != null ? ctx : FacesContext.getCurrentInstance();
     }
 
     protected String getFacetName()
     {
-        throw new UnsupportedOperationException("1.2");
+        if( parent == null || ! ( parent instanceof FacetTag ) )
+            return null;
+        else
+            return ((FacetTag)parent).getName();
+    }
+    
+    public boolean getCreated()
+    {
+        return created;
+    }
+    
+    protected int getIndexOfNextChildTag()
+    {
+        return getChildren().size();
+    }
+    
+    protected List<String> getCreatedComponents()
+    {
+        return children;
     }
     
     public JspWriter getPreviousOut()
     {
-        throw new UnsupportedOperationException("1.2");
+        if( bodyContent == null)
+            throw new NullPointerException("BodyContent");
+        
+        return bodyContent.getEnclosingWriter();
     }
     
-    //private UIComponent createChild(FacesContext context, UIComponent parent,
-    //        String componentId) throws JspException
-    //{
-    //    
-    //    throw new UnsupportedOperationException("1.2");
-    //
-    //}
+    private List<String> getChildren(){
+        if( children == null)
+            children = new ArrayList<String>();
+        return children;
+    }
+    
+    protected void addChild(UIComponent child)
+    {
+        getChildren().add(child.getId());
+    }
+
+    private List<String> getFacets(){
+        if( facets == null)
+            facets = new ArrayList<String>();
+        return facets;
+    }
+    
+    protected void addFacet(String name)
+    {
+        getFacets().add(name);
+    }
     
     public void setBodyContent(BodyContent bodyContent)
     {
