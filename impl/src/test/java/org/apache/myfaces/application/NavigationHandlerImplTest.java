ff5d2f1eee73e90c3b138d10fa82390924731ea3
Matthias Wessendorf <matzew@apache.org>
2006-06-21 05:05:04 +0000

    typos

--- impl/src/test/java/org/apache/myfaces/application/NavigationHandlerImplTest.java
+++ impl/src/test/java/org/apache/myfaces/application/NavigationHandlerImplTest.java
@@ -3,14 +3,13 @@ package org.apache.myfaces.application;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.apache.myfaces.application.jsp.JspStateManagerImplTest;
 import org.apache.shale.test.base.AbstractJsfTestCase;
 
 public class NavigationHandlerImplTest extends AbstractJsfTestCase
 {
 
 	    public static void main(String[] args) {
-	        junit.textui.TestRunner.run(JspStateManagerImplTest.class);
+	        junit.textui.TestRunner.run(NavigationHandlerImplTest.class);
 	    }
 
 	    public NavigationHandlerImplTest(String name) {
@@ -19,12 +18,13 @@ public class NavigationHandlerImplTest extends AbstractJsfTestCase
 	    
 	    public void testNavigationRules() throws Exception
 	    {
-	    	//TODO
+	    	NavigationHandlerImpl nh = new NavigationHandlerImpl();
+	    	this.application.setNavigationHandler(nh);
 	    }
 
 	    // Return the tests included in this test case.
 	    public static Test suite()
 	    {
-	        return (new TestSuite(JspStateManagerImplTest.class));
+	        return (new TestSuite(NavigationHandlerImplTest.class));
 	    }
 }
