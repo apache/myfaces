915a68d6e4b00bb90cd86ec9463f9187146efb35
Matthias Wessendorf <matzew@apache.org>
2006-07-20 16:11:16 +0000

    test case for UIData

--- api/src/test/java/javax/faces/component/InvokeOnComponentTest.java
+++ api/src/test/java/javax/faces/component/InvokeOnComponentTest.java
@@ -1,8 +1,16 @@
 package javax.faces.component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.faces.model.ScalarDataModel;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.apache.myfaces.dummy.data.Data;
 import org.jmock.Mock;
 
 public class InvokeOnComponentTest extends AbstractComponentTest
@@ -56,10 +64,81 @@ public class InvokeOnComponentTest extends AbstractComponentTest
     this.facesContext.getViewRoot().invokeOnComponent(facesContext, i2.getClientId(facesContext), cc);
     
   }
-  
+
+  public void testInvokeOnCompOnUIData() throws Exception
+  {
+    //column1
+    UIColumn c1 = new UIColumn();
+    c1.setId("col1");
+    
+    UIOutput headerFacet = new UIOutput();
+    headerFacet.setValue("HEADER");
+    headerFacet.setId("header");
+    c1.setHeader(headerFacet);
+    
+    UIOutput name = new UIOutput();
+    name.setValue("#{data.username}");
+    c1.getChildren().add(name);
+    
+    //column2
+    UIColumn c2 = new UIColumn();
+    c2.setId("col2");
+    
+    UIOutput secondheaderFacet = new UIOutput();
+    secondheaderFacet.setValue("New HEADER");
+    secondheaderFacet.setId("header2");
+    c2.setHeader(secondheaderFacet);
+    
+    UIOutput passwd = new UIOutput();
+    passwd.setValue("#{data.password}");
+    c2.getChildren().add(passwd);
+    
+    //main table
+    UIData table = new UIData();
+    table.setId("table");
+    
+    table.setVar("data");
+    
+    table.getChildren().add(c1);
+    table.getChildren().add(c2);
+
+    DataModel model = new ListDataModel(createTestData());
+    table.setValue(model);
+    this.facesContext.getViewRoot().getChildren().add(table);
+    
+    //there should be no call on passwd yet, b/c for UIData the invokeOnComp isn't implemented yet...
+    mock.expects(never()).method("invokeContextCallback").with(eq(facesContext), eq(passwd));
+    mock.expects(never()).method("invokeContextCallback").with(eq(facesContext), eq(table));
+    mock.expects(never()).method("invokeContextCallback").with(eq(facesContext), eq(c1));
+    mock.expects(never()).method("invokeContextCallback").with(eq(facesContext), eq(name));
+
+    this.facesContext.getViewRoot().invokeOnComponent(facesContext, passwd.getClientId(facesContext), cc);
+    
+  }
+
   public static Test suite()
   {
     return new TestSuite(InvokeOnComponentTest.class);
   }
+  
+  protected List<Data> createTestData()
+  {
+    List<Data> data = new ArrayList<Data>();
+    
+    
+    Data d1 = new Data();
+    d1.setPassword("secret");
+    d1.setUsername("mr fumakilla");
+    Data d2 = new Data();
+    d2.setPassword("top secret");
+    d2.setUsername("mr funk");
+    
+    data.add(d1);
+    data.add(d2);
+    
+    return data;
+  }
+  
+
   
 }
---
+++ api/src/test/java/org/apache/myfaces/dummy/data/Data.java
@@ -0,0 +1,26 @@
+package org.apache.myfaces.dummy.data;
+
+public class Data
+{
+  private String username, password;
+
+  public String getPassword()
+  {
+    return password;
+  }
+
+  public void setPassword(String password)
+  {
+    this.password = password;
+  }
+
+  public String getUsername()
+  {
+    return username;
+  }
+
+  public void setUsername(String username)
+  {
+    this.username = username;
+  }
+}
