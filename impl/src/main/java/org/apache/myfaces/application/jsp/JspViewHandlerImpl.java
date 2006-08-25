ffd6d1c060fbb6e193d95487c821b02bd5045455
Thomas Spiegl <tomsp@apache.org>
2006-08-25 13:22:46 +0000

    MYFACES-1393

--- impl/src/main/java/org/apache/myfaces/application/jsp/JspViewHandlerImpl.java
+++ impl/src/main/java/org/apache/myfaces/application/jsp/JspViewHandlerImpl.java
@@ -22,6 +22,8 @@ import org.apache.myfaces.portlet.PortletUtil;
 import org.apache.myfaces.util.DebugUtils;
 import org.apache.myfaces.shared_impl.webapp.webxml.ServletMapping;
 import org.apache.myfaces.shared_impl.webapp.webxml.WebXml;
+import org.apache.myfaces.shared_impl.renderkit.html.HTML;
+import org.apache.myfaces.shared_impl.renderkit.RendererUtils;
 
 import javax.faces.FacesException;
 import javax.faces.application.Application;
@@ -29,6 +31,7 @@ import javax.faces.application.ViewHandler;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 import javax.faces.render.RenderKitFactory;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderResponse;
@@ -46,37 +49,30 @@ import java.util.Locale;
  * @version $Revision$ $Date$
  */
 public class JspViewHandlerImpl
-        extends ViewHandler
-{
+    extends ViewHandler {
     private static final Log log = LogFactory.getLog(JspViewHandlerImpl.class);
-    public static final String FORM_STATE_MARKER     = "<!--@@JSF_FORM_STATE_MARKER@@-->";
-    public static final int    FORM_STATE_MARKER_LEN = FORM_STATE_MARKER.length();
+    public static final String FORM_STATE_MARKER = "<!--@@JSF_FORM_STATE_MARKER@@-->";
+    public static final int FORM_STATE_MARKER_LEN = FORM_STATE_MARKER.length();
 
 
-    public JspViewHandlerImpl()
-    {
+    public JspViewHandlerImpl() {
         if (log.isTraceEnabled()) log.trace("New ViewHandler instance created");
     }
 
-    public Locale calculateLocale(FacesContext facesContext)
-    {
+    public Locale calculateLocale(FacesContext facesContext) {
         Iterator locales = facesContext.getExternalContext().getRequestLocales();
-        while (locales.hasNext())
-        {
-            Locale locale = (Locale)locales.next();
-            for (Iterator it = facesContext.getApplication().getSupportedLocales(); it.hasNext();)
-            {
-                Locale supportLocale = (Locale)it.next();
+        while (locales.hasNext()) {
+            Locale locale = (Locale) locales.next();
+            for (Iterator it = facesContext.getApplication().getSupportedLocales(); it.hasNext();) {
+                Locale supportLocale = (Locale) it.next();
                 // higher priority to a language match over an exact match
                 // that occures further down (see Jstl Reference 1.0 8.3.1)
                 if (locale.getLanguage().equals(supportLocale.getLanguage()) &&
                     (supportLocale.getCountry() == null ||
-                     supportLocale.getCountry().length() == 0))
-                {
+                        supportLocale.getCountry().length() == 0)) {
                     return supportLocale;
                 }
-                else if (supportLocale.equals(locale))
-                {
+                else if (supportLocale.equals(locale)) {
                     return supportLocale;
                 }
             }
@@ -86,31 +82,28 @@ public class JspViewHandlerImpl
         return defaultLocale != null ? defaultLocale : Locale.getDefault();
     }
 
-    public String calculateRenderKitId(FacesContext facesContext)
-    {
-    	String renderKitId = facesContext.getApplication().getDefaultRenderKitId();
-        return (renderKitId!=null) ? renderKitId : RenderKitFactory.HTML_BASIC_RENDER_KIT;
+    public String calculateRenderKitId(FacesContext facesContext) {
+        String renderKitId = facesContext.getApplication().getDefaultRenderKitId();
+        return (renderKitId != null) ? renderKitId : RenderKitFactory.HTML_BASIC_RENDER_KIT;
         //TODO: how to calculate from client?
     }
 
     /**
      */
-    public UIViewRoot createView(FacesContext facesContext, String viewId)
-    {
+    public UIViewRoot createView(FacesContext facesContext, String viewId) {
         Application application = facesContext.getApplication();
         ViewHandler applicationViewHandler = application.getViewHandler();
 
         Locale currentLocale = null;
         String currentRenderKitId = null;
         UIViewRoot uiViewRoot = facesContext.getViewRoot();
-        if (uiViewRoot != null)
-        {
+        if (uiViewRoot != null) {
             //Remember current locale and renderKitId
             currentLocale = uiViewRoot.getLocale();
             currentRenderKitId = uiViewRoot.getRenderKitId();
         }
 
-        uiViewRoot = (UIViewRoot)application.createComponent(UIViewRoot.COMPONENT_TYPE);
+        uiViewRoot = (UIViewRoot) application.createComponent(UIViewRoot.COMPONENT_TYPE);
 //      as of JSF spec page 7-16:
 //      "It is the callers responsibility to ensure that setViewId() is called
 //      on the returned view, passing the same viewId value."
@@ -119,24 +112,20 @@ public class JspViewHandlerImpl
 //      ok, but the RI does so, so let's do it, too.
         uiViewRoot.setViewId(viewId);
 
-        if (currentLocale != null)
-        {
+        if (currentLocale != null) {
             //set old locale
             uiViewRoot.setLocale(currentLocale);
         }
-        else
-        {
+        else {
             //calculate locale
             uiViewRoot.setLocale(applicationViewHandler.calculateLocale(facesContext));
         }
 
-        if (currentRenderKitId != null)
-        {
+        if (currentRenderKitId != null) {
             //set old renderKit
             uiViewRoot.setRenderKitId(currentRenderKitId);
         }
-        else
-        {
+        else {
             //calculate renderKit
             uiViewRoot.setRenderKitId(applicationViewHandler.calculateRenderKitId(facesContext));
         }
@@ -145,44 +134,35 @@ public class JspViewHandlerImpl
         return uiViewRoot;
     }
 
-    public String getActionURL(FacesContext facesContext, String viewId)
-    {
-        if (PortletUtil.isRenderResponse(facesContext))
-        {
-            RenderResponse response = (RenderResponse)facesContext.getExternalContext().getResponse();
+    public String getActionURL(FacesContext facesContext, String viewId) {
+        if (PortletUtil.isRenderResponse(facesContext)) {
+            RenderResponse response = (RenderResponse) facesContext.getExternalContext().getResponse();
             PortletURL url = response.createActionURL();
             url.setParameter(MyFacesGenericPortlet.VIEW_ID, viewId);
             return url.toString();
         }
 
         String path = getViewIdPath(facesContext, viewId);
-        if (path.length() > 0 && path.charAt(0) == '/')
-        {
+        if (path.length() > 0 && path.charAt(0) == '/') {
             return facesContext.getExternalContext().getRequestContextPath() + path;
         }
-        else
-        {
+        else {
             return path;
         }
     }
 
-    public String getResourceURL(FacesContext facesContext, String path)
-    {
-        if (path.length() > 0 && path.charAt(0) == '/')
-        {
+    public String getResourceURL(FacesContext facesContext, String path) {
+        if (path.length() > 0 && path.charAt(0) == '/') {
             return facesContext.getExternalContext().getRequestContextPath() + path;
         }
-        else
-        {
+        else {
             return path;
         }
     }
 
     public void renderView(FacesContext facesContext, UIViewRoot viewToRender)
-            throws IOException, FacesException
-    {
-        if (viewToRender == null)
-        {
+        throws IOException, FacesException {
+        if (viewToRender == null) {
             log.fatal("viewToRender must not be null");
             throw new NullPointerException("viewToRender must not be null");
         }
@@ -198,22 +178,19 @@ public class JspViewHandlerImpl
 
         ServletMapping servletMapping = getServletMapping(externalContext);
 
-        if (servletMapping != null && servletMapping.isExtensionMapping())
-        {
+        if (servletMapping != null && servletMapping.isExtensionMapping()) {
             String defaultSuffix = externalContext.getInitParameter(ViewHandler.DEFAULT_SUFFIX_PARAM_NAME);
             String suffix = defaultSuffix != null ? defaultSuffix : ViewHandler.DEFAULT_SUFFIX;
             DebugUtils.assertError(suffix.charAt(0) == '.',
                                    log, "Default suffix must start with a dot!");
-            if (!viewId.endsWith(suffix))
-            {
+            if (!viewId.endsWith(suffix)) {
                 int dot = viewId.lastIndexOf('.');
-                if (dot == -1)
-                {
-                    if (log.isTraceEnabled()) log.trace("Current viewId has no extension, appending default suffix " + suffix);
+                if (dot == -1) {
+                    if (log.isTraceEnabled())
+                        log.trace("Current viewId has no extension, appending default suffix " + suffix);
                     viewId = viewId + suffix;
                 }
-                else
-                {
+                else {
                     if (log.isTraceEnabled()) log.trace("Replacing extension of current viewId by suffix " + suffix);
                     viewId = viewId.substring(0, dot) + suffix;
                 }
@@ -247,8 +224,7 @@ public class JspViewHandlerImpl
     }
 
 
-    public UIViewRoot restoreView(FacesContext facesContext, String viewId)
-    {
+    public UIViewRoot restoreView(FacesContext facesContext, String viewId) {
         Application application = facesContext.getApplication();
         ViewHandler applicationViewHandler = application.getViewHandler();
         String renderKitId = applicationViewHandler.calculateRenderKitId(facesContext);
@@ -261,85 +237,77 @@ public class JspViewHandlerImpl
     /**
      * Writes a state marker that is replaced later by one or more hidden form
      * inputs.
+     *
      * @param facesContext
      * @throws IOException
      */
-    public void writeState(FacesContext facesContext) throws IOException
-    {
-        if (facesContext.getApplication().getStateManager().isSavingStateInClient(facesContext))
-        {
+    public void writeState(FacesContext facesContext) throws IOException {
+        if (facesContext.getApplication().getStateManager().isSavingStateInClient(facesContext)) {
             facesContext.getResponseWriter().write(FORM_STATE_MARKER);
+        }
+        else {
+            ResponseWriter writer = facesContext.getResponseWriter();
+            writer.startElement(HTML.INPUT_ELEM, null);
+            writer.writeAttribute(HTML.TYPE_ATTR, HTML.INPUT_TYPE_HIDDEN, null);
+            writer.writeAttribute(HTML.NAME_ATTR, RendererUtils.SEQUENCE_PARAM, null);
+            writer.writeAttribute(HTML.VALUE_ATTR, RendererUtils.getViewSequence(facesContext), null);
+            writer.endElement(HTML.INPUT_ELEM);
         }
     }
 
 
-    protected String getViewIdPath(FacesContext facescontext, String viewId)
-    {
-        if (viewId == null)
-        {
+    protected String getViewIdPath(FacesContext facescontext, String viewId) {
+        if (viewId == null) {
             log.error("ViewId must not be null");
             throw new NullPointerException("ViewId must not be null");
         }
-        if (!viewId.startsWith("/"))
-        {
+        if (!viewId.startsWith("/")) {
             log.error("ViewId must start with '/' (viewId = " + viewId + ")");
             throw new IllegalArgumentException("ViewId must start with '/' (viewId = " + viewId + ")");
         }
 
-        if (PortletUtil.isPortletRequest(facescontext))
-        {
+        if (PortletUtil.isPortletRequest(facescontext)) {
             return viewId;
         }
 
         ServletMapping servletMapping = getServletMapping(facescontext.getExternalContext());
 
-        if (servletMapping !=null)
-        {
-            if(servletMapping.isExtensionMapping())
-            {
+        if (servletMapping != null) {
+            if (servletMapping.isExtensionMapping()) {
                 // extension mapping
                 String urlpattern = servletMapping.getUrlPattern();
-                if (urlpattern.startsWith("*"))
-                {
+                if (urlpattern.startsWith("*")) {
                     urlpattern = urlpattern.substring(1, urlpattern.length());
                 }
-                if (viewId.endsWith(urlpattern))
-                {
+                if (viewId.endsWith(urlpattern)) {
                     return viewId;
                 }
-                else
-                {
+                else {
                     int idx = viewId.lastIndexOf(".");
-                    if (idx >= 0)
-                    {
+                    if (idx >= 0) {
                         return viewId.substring(0, idx) + urlpattern;
                     }
-                    else
-                    {
+                    else {
                         return viewId + urlpattern;
                     }
 
                 }
             }
-            else
-            {
+            else {
                 // prefix mapping
                 String urlpattern = servletMapping.getUrlPattern();
-                if (urlpattern.endsWith("/*"))
-                {
+                if (urlpattern.endsWith("/*")) {
                     urlpattern = urlpattern.substring(0, urlpattern.length() - 2);
                 }
                 return urlpattern + viewId;
             }
         }
-        else
-        {
+        else {
             return viewId;
         }
     }
 
-    private static ServletMapping getServletMapping(ExternalContext externalContext)
-    {
+    private static ServletMapping getServletMapping(ExternalContext externalContext) {
         String servletPath = externalContext.getRequestServletPath();
         String requestPathInfo = externalContext.getRequestPathInfo();
 
@@ -347,28 +315,24 @@ public class JspViewHandlerImpl
         List mappings = webxml.getFacesServletMappings();
 
 
-        if (requestPathInfo == null)
-        {
+        if (requestPathInfo == null) {
             // might be extension mapping
-            for (int i = 0, size = mappings.size(); i < size; i++)
-            {
+            for (int i = 0, size = mappings.size(); i < size; i++) {
                 ServletMapping servletMapping = (ServletMapping) mappings.get(i);
                 String urlpattern = servletMapping.getUrlPattern();
                 String extension = urlpattern.substring(1, urlpattern.length());
-                if (servletPath.endsWith(extension))
-                {
+                if (servletPath.endsWith(extension)) {
                     return servletMapping;
-                } else if (servletPath.equals(urlpattern)) {
+                }
+                else if (servletPath.equals(urlpattern)) {
                     // path mapping with no pathInfo for the current request
                     return servletMapping;
                 }
             }
         }
-        else
-        {
+        else {
             // path mapping
-            for (int i = 0, size = mappings.size(); i < size; i++)
-            {
+            for (int i = 0, size = mappings.size(); i < size; i++) {
 
                 ServletMapping servletMapping = (ServletMapping) mappings.get(i);
                 String urlpattern = servletMapping.getUrlPattern();
@@ -376,8 +340,7 @@ public class JspViewHandlerImpl
                 // servletPath starts with "/" except in the case where the
                 // request is matched with the "/*" pattern, in which case
                 // it is the empty string (see Servlet Sepc 2.3 SRV4.4)
-                if (servletPath.equals(urlpattern))
-                {
+                if (servletPath.equals(urlpattern)) {
                     return servletMapping;
                 }
             }
@@ -385,12 +348,10 @@ public class JspViewHandlerImpl
 
         // handle cases as best possible where servletPath is not a faces servlet,
         // such as when coming through struts-faces
-        if (mappings.size() > 0)
-        {
+        if (mappings.size() > 0) {
             return (ServletMapping) mappings.get(0);
         }
-        else
-        {
+        else {
             log.warn("no faces servlet mappings found");
             return null;
         }
