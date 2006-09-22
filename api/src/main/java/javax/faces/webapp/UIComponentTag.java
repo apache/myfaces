f3adb70ab534294512ba93050106565cc5d96eca
Martin Marinschek <mmarinschek@apache.org>
2006-09-22 13:16:37 +0000

    Changed the message the users sees when facesContext is null

--- api/src/main/java/javax/faces/webapp/UIComponentTag.java
+++ api/src/main/java/javax/faces/webapp/UIComponentTag.java
@@ -923,7 +923,7 @@ public abstract class UIComponentTag
             throw new FacesException("Faces context not found. getResponseWriter will fail. "+
                     "Check if the FacesServlet has been initialized at all in your web.xml configuration file"+
                     "and if you are accessing your jsf-pages through the correct mapping. E.g.: if your FacesServlet is mapped to "+
-                    " *.jsf, you need to access your pages as 'sample.jsf'. If you'd try to access 'sample.jsp', you'd get this error-message."                    
+                    " *.jsf (with the <servlet-mapping>-element), you need to access your pages as 'sample.jsf'. If you tried to access 'sample.jsp', you'd get this error-message."                    
                     );
         }
 
