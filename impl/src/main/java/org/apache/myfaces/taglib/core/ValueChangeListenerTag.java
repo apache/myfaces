e1a8a0b384138b4baf3e860ea22ed75aab9f67d8
Bruno Aranda Delgado <baranda@apache.org>
2006-10-26 12:04:43 +0000

    Applied patch for MYFACES-1462 (Implement JSR-252 core tag: ValueChangeListenerTag). Thanks to Andreas Berger

--- impl/src/main/java/org/apache/myfaces/taglib/core/ValueChangeListenerTag.java
+++ impl/src/main/java/org/apache/myfaces/taglib/core/ValueChangeListenerTag.java
@@ -15,82 +15,25 @@
  */
 package org.apache.myfaces.taglib.core;
 
-import org.apache.myfaces.shared_impl.util.ClassUtils;
-
 import javax.faces.component.EditableValueHolder;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.el.ValueBinding;
 import javax.faces.event.ValueChangeListener;
-import javax.faces.webapp.UIComponentTag;
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.Tag;
-import javax.servlet.jsp.tagext.TagSupport;
 
 /**
  * @author Manfred Geiler (latest modification by $Author$)
  * @version $Revision$ $Date$
  */
 public class ValueChangeListenerTag
-        extends TagSupport
+        extends GenericListenerTag<EditableValueHolder, ValueChangeListener>
 {
-    //private static final Log log = LogFactory.getLog(ValueChangeListenerTag.class);
-
     private static final long serialVersionUID = 2155190261951046892L;
-    private String _type = null;
 
     public ValueChangeListenerTag()
     {
+        super(EditableValueHolder.class);
     }
 
-    public void setType(String type)
+    protected void addListener(EditableValueHolder editableValueHolder, ValueChangeListener valueChangeListener)
     {
-        _type = type;
+        editableValueHolder.addValueChangeListener(valueChangeListener);
     }
-
-
-    public int doStartTag() throws JspException
-    {
-        if (_type == null)
-        {
-            throw new JspException("type attribute not set");
-        }
-
-        //Find parent UIComponentTag
-        UIComponentTag componentTag = UIComponentTag.getParentUIComponentTag(pageContext);
-        if (componentTag == null)
-        {
-            throw new JspException("ValueChangeListenerTag has no UIComponentTag ancestor");
-        }
-
-        if (componentTag.getCreated())
-        {
-            //Component was just created, so we add the Listener
-            UIComponent component = componentTag.getComponentInstance();
-            if (component instanceof EditableValueHolder)
-            {
-                String className;
-                if (UIComponentTag.isValueReference(_type))
-                {
-                    FacesContext facesContext = FacesContext.getCurrentInstance();
-                    ValueBinding vb = facesContext.getApplication().createValueBinding(_type);
-                    className = (String)vb.getValue(facesContext);
-                }
-                else
-                {
-                    className = _type;
-                }
-                ValueChangeListener vcl = (ValueChangeListener)ClassUtils.newInstance(className);
-                ((EditableValueHolder)component).addValueChangeListener(vcl);
-            }
-            else
-            {
-                throw new JspException("Component " + component.getId() + " is no EditableValueHolder");
-            }
-        }
-
-        return Tag.SKIP_BODY;
-    }
-
-
 }
