04275c19f56c36d77c7d85f1083e4bcdd9c6373c
Bruno Aranda Delgado <baranda@apache.org>
2006-11-05 18:39:29 +0000

    MyFaces 1.2, first setup for the component autogeneration using the maven-faces-plugin from trinidad (using a snapshot now from a branched version of that plugin, that is able to generate to myfaces components classes, tags and configuration files). Added myfaces-build module that contains the XML that define the components. First component autogenerated: UICommand, clap clap clap :-) Deleted old version Still some issues with component and tag generation, but now it is possible to start writing the component xmls.

---
+++ api/src/main/java-templates/javax/faces/component/UICommandTemplate.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package javax.faces.component;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+import javax.faces.event.FacesEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.el.MethodBinding;
+import javax.el.MethodExpression;
+
+/**
+ * TODO: comment this!
+ *
+ * @author Bruno Aranda (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public abstract class UICommandTemplate extends UIComponentBase
+{
+    /**/ // lines starting with /**/ are magically ignored from the generated class
+
+    /**/public abstract boolean isImmediate();
+    /**/public abstract ActionListener[] getActionListeners();
+    /**/public abstract MethodExpression getActionExpression();
+    /**/public abstract void setActionExpression(MethodExpression methodExpression);
+
+    /**
+     * @deprecated Use setActionExpression instead.
+     */
+    public void setAction(MethodBinding action)
+    {
+        setActionExpression(new _MethodBindingToMethodExpression(action));
+    }
+
+    /**
+     * @deprecated Use getActionExpression() instead.
+     */
+    public MethodBinding getAction()
+    {
+        MethodExpression actionExpression = getActionExpression();
+        if (actionExpression instanceof _MethodBindingToMethodExpression) {
+            return ((_MethodBindingToMethodExpression)actionExpression).getMethodBinding();
+        }
+
+        return new _MethodExpressionToMethodBinding(actionExpression);
+    }
+
+    public void broadcast(FacesEvent event)
+            throws AbortProcessingException
+    {
+        super.broadcast(event);
+
+        if (event instanceof ActionEvent)
+        {
+            FacesContext context = getFacesContext();
+
+            for (ActionListener listener : getActionListeners())
+            {
+                listener.processAction((ActionEvent)event);
+            }
+
+            ActionListener defaultActionListener
+                    = context.getApplication().getActionListener();
+            if (defaultActionListener != null)
+            {
+                defaultActionListener.processAction((ActionEvent)event);
+            }
+        }
+    }
+
+    public void queueEvent(FacesEvent event)
+    {
+        if (event != null && event instanceof ActionEvent)
+        {
+            if (isImmediate())
+            {
+                event.setPhaseId(PhaseId.APPLY_REQUEST_VALUES);
+            }
+            else
+            {
+                event.setPhaseId(PhaseId.INVOKE_APPLICATION);
+            }
+        }
+        super.queueEvent(event);
+    }
+}
