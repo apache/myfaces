2803ed0b2b04d620af0eb6039461062a806ba02b
Thomas Spiegl <tomsp@apache.org>
2006-11-07 13:59:53 +0000

    MYFACES-1418 reverted due to JSF 1.1 incompatibility

--- api/src/main/java/javax/faces/validator/LengthValidator.java
+++ api/src/main/java/javax/faces/validator/LengthValidator.java
@@ -15,6 +15,10 @@
  */
 package javax.faces.validator;
 
+import javax.faces.component.StateHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
 /**
  * see Javadoc of <a href="http://java.sun.com/j2ee/javaserverfaces/1.1_01/docs/api/index.html">JSF Specification</a>
  *
@@ -22,68 +26,131 @@ package javax.faces.validator;
  * @author Thomas Spiegl
  * @version $Revision$ $Date$
  */
-public class LengthValidator extends NumberRangeValidator {
-	// FIELDS
-	public static final String MAXIMUM_MESSAGE_ID = "javax.faces.validator.LengthValidator.MAXIMUM";
-	public static final String MINIMUM_MESSAGE_ID = "javax.faces.validator.LengthValidator.MINIMUM";
-	public static final String VALIDATOR_ID = "javax.faces.Length";
+public class LengthValidator
+        implements Validator, StateHolder
+{
+    // FIELDS
+    public static final String 	MAXIMUM_MESSAGE_ID = "javax.faces.validator.LengthValidator.MAXIMUM";
+    public static final String 	MINIMUM_MESSAGE_ID = "javax.faces.validator.LengthValidator.MINIMUM";
+    public static final String 	VALIDATOR_ID 	   = "javax.faces.Length";
 
-	// CONSTRUCTORS
-	public LengthValidator() {
-		super(null, null, false);
-	}
+    private Integer _minimum = null;
+    private Integer _maximum = null;
+    private boolean _transient = false;
 
-	public LengthValidator(int maximum) {
-		super(null, new Integer(maximum), false);
-	}
+    // CONSTRUCTORS
+    public LengthValidator()
+    {
+    }
 
-	public LengthValidator(int maximum, int minimum) {
-		super(new Integer(minimum), new Integer(maximum), false);
-	}
+    public LengthValidator(int maximum)
+    {
+        _maximum = new Integer(maximum);
+    }
 
-	// Abstract methods implementation
-	protected Comparable getValue(Number number) {
-		return new Integer(number.toString().length());
-	}
+    public LengthValidator(int maximum,
+                           int minimum)
+    {
+        _maximum = new Integer(maximum);
+        _minimum = new Integer(minimum);
+    }
 
-	protected Comparable getValue(String str) throws NumberFormatException {
-		return new Integer(str.length());
-	}
+    // VALIDATE
+    public void validate(FacesContext facesContext,
+                         UIComponent uiComponent,
+                         Object value)
+            throws ValidatorException
+    {
+        if (facesContext == null) throw new NullPointerException("facesContext");
+        if (uiComponent == null) throw new NullPointerException("uiComponent");
 
-	protected String getMaximumMessageId() {
-		return MAXIMUM_MESSAGE_ID;
-	}
+        if (value == null)
+        {
+            return;
+        }
 
-	protected String getMinimumMessageId() {
-		return MINIMUM_MESSAGE_ID;
-	}
+        int length = value instanceof String ?
+            ((String)value).length() : value.toString().length();
 
-	protected String getTypeMessageId() {
-		return null;
-	}
+        if (_minimum != null)
+        {
+            if (length < _minimum.intValue())
+            {
+                Object[] args = {_minimum,uiComponent.getId()};
+                throw new ValidatorException(_MessageUtils.getErrorMessage(facesContext, MINIMUM_MESSAGE_ID, args));
+            }
+        }
 
-	// SETTER & GETTER
-	public int getMaximum() {
-		return _maximum != null ? _maximum.intValue() : Integer.MAX_VALUE;
-	}
+        if (_maximum != null)
+        {
+            if (length > _maximum.intValue())
+            {
+                Object[] args = {_maximum,uiComponent.getId()};
+                throw new ValidatorException(_MessageUtils.getErrorMessage(facesContext, MAXIMUM_MESSAGE_ID, args));
+            }
+        }
+    }
 
-	public void setMaximum(int maximum) {
-		_maximum = new Integer(maximum);
-	}
+    // SETTER & GETTER
+    public int getMaximum()
+    {
+        return _maximum != null ? _maximum.intValue() : Integer.MAX_VALUE;
+    }
 
-	public int getMinimum() {
-		return _minimum != null ? _minimum.intValue() : 0;
-	}
+    public void setMaximum(int maximum)
+    {
+        _maximum = new Integer(maximum);
+    }
 
-	public void setMinimum(int minimum) {
-		_minimum = new Integer(minimum);
-	}
+    public int getMinimum()
+    {
+        return _minimum != null ? _minimum.intValue() : 0;
+    }
 
-	// MISC
-	public boolean equals(Object o) {
-		if (!(o instanceof LengthValidator)) {
-			return false;
-		}
-		return super.equals(o);
-	}
+    public void setMinimum(int minimum)
+    {
+        _minimum = new Integer(minimum);
+    }
+
+    public boolean isTransient()
+    {
+        return _transient;
+    }
+
+    public void setTransient(boolean transientValue)
+    {
+        _transient = transientValue;
+    }
+
+    // RESTORE & SAVE STATE
+    public Object saveState(FacesContext context)
+    {
+        Object values[] = new Object[2];
+        values[0] = _maximum;
+        values[1] = _minimum;
+        return values;
+    }
+
+    public void restoreState(FacesContext context,
+                             Object state)
+    {
+        Object values[] = (Object[])state;
+        _maximum = (Integer)values[0];
+        _minimum = (Integer)values[1];
+    }
+
+    // MISC
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof LengthValidator)) return false;
+
+        final LengthValidator lengthValidator = (LengthValidator)o;
+
+        if (_maximum != null ? !_maximum.equals(lengthValidator._maximum) : lengthValidator._maximum != null) return false;
+        if (_minimum != null ? !_minimum.equals(lengthValidator._minimum) : lengthValidator._minimum != null) return false;
+
+        return true;
+    }
+
 }
--- api/src/main/java/javax/faces/validator/LongRangeValidator.java
+++ api/src/main/java/javax/faces/validator/LongRangeValidator.java
@@ -15,79 +15,171 @@
  */
 package javax.faces.validator;
 
+import javax.faces.component.StateHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
 /**
  * see Javadoc of <a href="http://java.sun.com/j2ee/javaserverfaces/1.1_01/docs/api/index.html">JSF Specification</a>
- * 
+ *
  * @author Manfred Geiler (latest modification by $Author$)
  * @author Thomas Spiegl
  * @version $Revision$ $Date$
  */
-public class LongRangeValidator extends NumberRangeValidator {
-	// FIELDS
-	public static final String MAXIMUM_MESSAGE_ID = "javax.faces.validator.LongRangeValidator.MAXIMUM";
-	public static final String MINIMUM_MESSAGE_ID = "javax.faces.validator.LongRangeValidator.MINIMUM";
-	public static final String TYPE_MESSAGE_ID = "javax.faces.validator.LongRangeValidator.TYPE";
-	public static final String VALIDATOR_ID = "javax.faces.LongRange";
+public class LongRangeValidator
+        implements Validator, StateHolder
+{
+    // FIELDS
+    public static final String MAXIMUM_MESSAGE_ID = "javax.faces.validator.LongRangeValidator.MAXIMUM";
+    public static final String MINIMUM_MESSAGE_ID =	"javax.faces.validator.LongRangeValidator.MINIMUM";
+    public static final String TYPE_MESSAGE_ID 	  = "javax.faces.validator.LongRangeValidator.TYPE";
+    public static final String VALIDATOR_ID 	  = "javax.faces.LongRange";
 
-	// CONSTRUCTORS
-	public LongRangeValidator() {
-		super(null, null);
-	}
+    private Long _minimum = null;
+    private Long _maximum = null;
+    private boolean _transient = false;
 
-	public LongRangeValidator(long maximum) {
-		super(null, new Long(maximum));
-	}
+    // CONSTRUCTORS
+    public LongRangeValidator()
+    {
+    }
 
-	public LongRangeValidator(long maximum, long minimum) {
-		super(new Long(minimum), new Long(maximum));
-	}
+    public LongRangeValidator(long maximum)
+    {
+        _maximum = new Long(maximum);
+    }
 
-	protected Comparable getValue(Number number) {
-		if(number instanceof Long) {
-			return (Long) number;
-		}
-		return new Long(number.longValue());
-	}
+    public LongRangeValidator(long maximum,
+                              long minimum)
+    {
+        _maximum = new Long(maximum);
+        _minimum = new Long(minimum);
+    }
 
-	// Abstract methods implementation
-	protected Comparable getValue(String str) throws NumberFormatException {
-		return new Long(str);
-	}
+    // VALIDATE
+    public void validate(FacesContext facesContext,
+                         UIComponent uiComponent,
+                         Object value)
+            throws ValidatorException
+    {
+        if (facesContext == null) throw new NullPointerException("facesContext");
+        if (uiComponent == null) throw new NullPointerException("uiComponent");
 
-	protected String getMaximumMessageId() {
-		return MAXIMUM_MESSAGE_ID;
-	}
+        if (value == null)
+        {
+            return;
+        }
 
-	protected String getMinimumMessageId() {
-		return MINIMUM_MESSAGE_ID;
-	}
+        double dvalue = parseLongValue(facesContext, uiComponent,value);
+        if (_minimum != null && _maximum != null)
+        {
+            if (dvalue < _minimum.longValue() ||
+                dvalue > _maximum.longValue())
+            {
+                Object[] args = {_minimum, _maximum,uiComponent.getId()};
+                throw new ValidatorException(_MessageUtils.getErrorMessage(facesContext, NOT_IN_RANGE_MESSAGE_ID, args));
+            }
+        }
+        else if (_minimum != null)
+        {
+            if (dvalue < _minimum.longValue())
+            {
+                Object[] args = {_minimum,uiComponent.getId()};
+                throw new ValidatorException(_MessageUtils.getErrorMessage(facesContext, MINIMUM_MESSAGE_ID, args));
+            }
+        }
+        else if (_maximum != null)
+        {
+            if (dvalue > _maximum.longValue())
+            {
+                Object[] args = {_maximum,uiComponent.getId()};
+                throw new ValidatorException(_MessageUtils.getErrorMessage(facesContext, MAXIMUM_MESSAGE_ID, args));
+            }
+        }
+    }
 
-	protected String getTypeMessageId() {
-		return TYPE_MESSAGE_ID;
-	}
+    private long parseLongValue(FacesContext facesContext, UIComponent uiComponent, Object value)
+        throws ValidatorException
+    {
+        if (value instanceof Number)
+        {
+            return ((Number)value).longValue();
+        }
+        else
+        {
+            try
+            {
+                return Long.parseLong(value.toString());
+            }
+            catch (NumberFormatException e)
+            {
+				Object[] args = {uiComponent.getId()};
+                throw new ValidatorException(_MessageUtils.getErrorMessage(facesContext, TYPE_MESSAGE_ID, args));
+            }
+        }
+    }
 
-	// GETTER & SETTER
-	public long getMaximum() {
-		return _maximum != null ? _maximum.longValue() : Long.MAX_VALUE;
-	}
 
-	public void setMaximum(long maximum) {
-		_maximum = new Long(maximum);
-	}
+     // GETTER & SETTER
+    public long getMaximum()
+    {
+        return _maximum != null ? _maximum.longValue() : Long.MAX_VALUE;
+    }
 
-	public long getMinimum() {
-		return _minimum != null ? _minimum.longValue() : Long.MIN_VALUE;
-	}
+    public void setMaximum(long maximum)
+    {
+        _maximum = new Long(maximum);
+    }
 
-	public void setMinimum(long minimum) {
-		_minimum = new Long(minimum);
-	}
+    public long getMinimum()
+    {
+        return _minimum != null ? _minimum.longValue() : Long.MIN_VALUE;
+    }
 
-	// MISC
-	public boolean equals(Object o) {
-		if (!(o instanceof LongRangeValidator)) {
-			return false;
-		}
-		return super.equals(o);
-	}
+    public void setMinimum(long minimum)
+    {
+        _minimum = new Long(minimum);
+    }
+
+    public boolean isTransient()
+    {
+        return _transient;
+    }
+
+    public void setTransient(boolean transientValue)
+    {
+        _transient = transientValue;
+    }
+
+    // RESTORE & SAVE STATE
+    public Object saveState(FacesContext context)
+    {
+        Object values[] = new Object[2];
+        values[0] = _maximum;
+        values[1] = _minimum;
+        return values;
+    }
+
+    public void restoreState(FacesContext context,
+                             Object state)
+    {
+        Object values[] = (Object[])state;
+        _maximum = (Long)values[0];
+        _minimum = (Long)values[1];
+    }
+
+    // MISC
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof LongRangeValidator)) return false;
+
+        final LongRangeValidator longRangeValidator = (LongRangeValidator)o;
+
+        if (_maximum != null ? !_maximum.equals(longRangeValidator._maximum) : longRangeValidator._maximum != null) return false;
+        if (_minimum != null ? !_minimum.equals(longRangeValidator._minimum) : longRangeValidator._minimum != null) return false;
+
+        return true;
+    }
+
 }
