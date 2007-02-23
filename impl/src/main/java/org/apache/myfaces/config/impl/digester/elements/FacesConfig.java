d06d26383be900a5f03b4568fd3bcda5a6257374
Mathias Brökelmann <mbr@apache.org>
2007-02-23 15:01:37 +0000

    inject generic types

--- impl/src/main/java/org/apache/myfaces/config/impl/digester/elements/FacesConfig.java
+++ impl/src/main/java/org/apache/myfaces/config/impl/digester/elements/FacesConfig.java
@@ -16,13 +16,9 @@
 package org.apache.myfaces.config.impl.digester.elements;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import org.apache.myfaces.config.impl.digester.elements.Application;
-import org.apache.myfaces.config.impl.digester.elements.Converter;
-
 
 /**
  * @author <a href="mailto:oliver@rossmueller.com">Oliver Rossmueller</a>
@@ -30,120 +26,102 @@ import org.apache.myfaces.config.impl.digester.elements.Converter;
 public class FacesConfig
 {
 
-    private List applications = new ArrayList();
-    private List factories = new ArrayList();
-    private Map components = new HashMap();
-    private List converters = new ArrayList();
-    private List managedBeans = new ArrayList();
-    private List navigationRules = new ArrayList();
-    private List renderKits = new ArrayList();
-    private List lifecyclePhaseListener = new ArrayList();
-    private Map validators = new HashMap();
-
+    private List<Application> applications = new ArrayList<Application>();
+    private List<Factory> factories = new ArrayList<Factory>();
+    private Map<String, String> components = new HashMap<String, String>();
+    private List<Converter> converters = new ArrayList<Converter>();
+    private List<ManagedBean> managedBeans = new ArrayList<ManagedBean>();
+    private List<NavigationRule> navigationRules = new ArrayList<NavigationRule>();
+    private List<RenderKit> renderKits = new ArrayList<RenderKit>();
+    private List<String> lifecyclePhaseListener = new ArrayList<String>();
+    private Map<String, String> validators = new HashMap<String, String>();
 
     public void addApplication(Application application)
     {
         applications.add(application);
     }
 
-
     public void addFactory(Factory factory)
     {
         factories.add(factory);
     }
-
 
     public void addComponent(String componentType, String componentClass)
     {
         components.put(componentType, componentClass);
     }
 
-
     public void addConverter(Converter converter)
     {
         converters.add(converter);
     }
-
 
     public void addManagedBean(ManagedBean bean)
     {
         managedBeans.add(bean);
     }
 
-
     public void addNavigationRule(NavigationRule rule)
     {
         navigationRules.add(rule);
     }
-
 
     public void addRenderKit(RenderKit renderKit)
     {
         renderKits.add(renderKit);
     }
 
-
     public void addLifecyclePhaseListener(String value)
     {
         lifecyclePhaseListener.add(value);
     }
-
 
     public void addValidator(String id, String validatorClass)
     {
         validators.put(id, validatorClass);
     }
 
-
-    public List getApplications()
+    public List<Application> getApplications()
     {
         return applications;
     }
 
-
-    public List getFactories()
+    public List<Factory> getFactories()
     {
         return factories;
     }
 
-
-    public Map getComponents()
+    public Map<String, String> getComponents()
     {
         return components;
     }
 
-
-    public List getConverters()
+    public List<Converter> getConverters()
     {
         return converters;
     }
 
-
-    public List getManagedBeans()
+    public List<ManagedBean> getManagedBeans()
     {
         return managedBeans;
     }
 
-
-    public List getNavigationRules()
+    public List<NavigationRule> getNavigationRules()
     {
         return navigationRules;
     }
 
-
-    public List getRenderKits()
+    public List<RenderKit> getRenderKits()
     {
         return renderKits;
     }
 
-
-    public List getLifecyclePhaseListener()
+    public List<String> getLifecyclePhaseListener()
     {
         return lifecyclePhaseListener;
     }
 
-
-    public Map getValidators()
+    public Map<String, String> getValidators()
     {
         return validators;
     }
