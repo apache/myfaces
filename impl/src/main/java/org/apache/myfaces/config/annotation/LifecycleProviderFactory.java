119f9bf2a9049fa4b52e56eb1e617c96e6959d9c
Matthias Wessendorf <matzew@apache.org>
2007-07-18 19:51:27 +0000

    license headers

--- impl/src/main/java/org/apache/myfaces/config/annotation/LifecycleProviderFactory.java
+++ impl/src/main/java/org/apache/myfaces/config/annotation/LifecycleProviderFactory.java
@@ -1,5 +1,3 @@
-package org.apache.myfaces.config.annotation;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,7 @@ package org.apache.myfaces.config.annotation;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.myfaces.config.annotation;
 
 import org.apache.commons.discovery.tools.DiscoverSingleton;
 
