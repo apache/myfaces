186aab8ea7a580e93d77c0f8a44db0eec792dd52
Leonardo Uribe <lu4242@apache.org>
2009-08-19 20:22:41 +0000

    MYFACES-2288 Implement Bean Validation

---
+++ impl/src/main/java/org/apache/myfaces/taglib/core/ValidateBeanTag.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.taglib.core;
+
+import javax.faces.validator.Validator;
+import javax.faces.webapp.ValidatorELTag;
+import javax.servlet.jsp.JspException;
+
+/**
+ * TODO: Implement me! 
+ */
+public class ValidateBeanTag extends ValidatorELTag
+{
+
+    @Override
+    protected Validator createValidator() throws JspException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
