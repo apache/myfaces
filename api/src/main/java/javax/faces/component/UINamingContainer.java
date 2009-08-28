296be61526b9ed84bdcd3315ab8a8dccb48de757
Leonardo Uribe <lu4242@apache.org>
2009-08-28 00:31:54 +0000

    MYFACES-2337 Implement UINamingContainer.createUniqueId method and review all related code to UniqueIdVendor

--- api/src/main/java/javax/faces/component/UINamingContainer.java
+++ api/src/main/java/javax/faces/component/UINamingContainer.java
@@ -64,8 +64,22 @@ public class UINamingContainer extends UIComponentBase implements NamingContaine
     @Override
     public String createUniqueId(FacesContext context, String seed)
     {
-        // TODO: IMPLEMENT HERE
-        return null;
+        ExternalContext extCtx = context.getExternalContext();
+        StringBuilder bld = __getSharedStringBuilder();
+
+        Long uniqueIdCounter = (Long) getStateHelper().get(PropertyKeys.uniqueIdCounter);
+        uniqueIdCounter = (uniqueIdCounter == null) ? 0 : uniqueIdCounter;
+        getStateHelper().put(PropertyKeys.uniqueIdCounter, (uniqueIdCounter+1L));
+        // Generate an identifier for a component. The identifier will be prefixed with UNIQUE_ID_PREFIX, and will be unique within this UIViewRoot. 
+        if(seed==null)
+        {
+            return extCtx.encodeNamespace(bld.append(UIViewRoot.UNIQUE_ID_PREFIX).append(uniqueIdCounter).toString());    
+        }
+        // Optionally, a unique seed value can be supplied by component creators which should be included in the generated unique id.
+        else
+        {
+            return extCtx.encodeNamespace(bld.append(UIViewRoot.UNIQUE_ID_PREFIX).append(seed).toString());
+        }
     }
     
     /**
@@ -94,5 +108,10 @@ public class UINamingContainer extends UIComponentBase implements NamingContaine
             // this method
             return param.charAt(0);
         }
+    }
+    
+    enum PropertyKeys
+    {
+        uniqueIdCounter
     }
 }
