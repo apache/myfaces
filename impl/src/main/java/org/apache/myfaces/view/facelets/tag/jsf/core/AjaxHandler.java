d06c7fc11958fb5fd59ea254e449b06ed615239c
Leonardo Uribe <lu4242@apache.org>
2009-08-31 19:22:59 +0000

    MYFACES-2323 Implement <f:ajax> tag handler

---
+++ impl/src/main/java/org/apache/myfaces/view/facelets/tag/jsf/core/AjaxHandler.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.view.facelets.tag.jsf.core;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.view.BehaviorHolderAttachedObjectHandler;
+import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.TagConfig;
+import javax.faces.view.facelets.TagHandler;
+
+import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFFaceletTag;
+
+/**
+ * @author Leonardo Uribe (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+@JSFFaceletTag(name="f:ajax")
+public class AjaxHandler extends TagHandler implements BehaviorHolderAttachedObjectHandler
+{
+
+    public AjaxHandler(TagConfig config)
+    {
+        super(config);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void apply(FaceletContext ctx, UIComponent parent)
+            throws IOException
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getEventName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void applyAttachedObject(FacesContext context, UIComponent parent)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getFor()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
