a50e20cdc2337be82bd87b0ff645892b2662e789
Leonardo Uribe <lu4242@apache.org>
2009-10-28 18:42:33 +0000

    MYFACES-2290 Add OSGi bundle information and bundle classloader / activator (Digester should use ClassUtils.getContextClassLoader() )

--- impl/src/main/java/org/apache/myfaces/config/impl/digester/DigesterFacesConfigUnmarshallerImpl.java
+++ impl/src/main/java/org/apache/myfaces/config/impl/digester/DigesterFacesConfigUnmarshallerImpl.java
@@ -21,12 +21,29 @@ package org.apache.myfaces.config.impl.digester;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+
 import javax.faces.context.ExternalContext;
 
-import org.apache.myfaces.config.FacesConfigUnmarshaller;
-import org.apache.myfaces.config.impl.digester.elements.*;
-import org.apache.myfaces.config.impl.FacesConfigEntityResolver;
 import org.apache.commons.digester.Digester;
+import org.apache.myfaces.config.FacesConfigUnmarshaller;
+import org.apache.myfaces.config.impl.FacesConfigEntityResolver;
+import org.apache.myfaces.config.impl.digester.elements.Application;
+import org.apache.myfaces.config.impl.digester.elements.Attribute;
+import org.apache.myfaces.config.impl.digester.elements.Converter;
+import org.apache.myfaces.config.impl.digester.elements.FacesConfig;
+import org.apache.myfaces.config.impl.digester.elements.Factory;
+import org.apache.myfaces.config.impl.digester.elements.ListEntries;
+import org.apache.myfaces.config.impl.digester.elements.LocaleConfig;
+import org.apache.myfaces.config.impl.digester.elements.ManagedBean;
+import org.apache.myfaces.config.impl.digester.elements.ManagedProperty;
+import org.apache.myfaces.config.impl.digester.elements.MapEntries;
+import org.apache.myfaces.config.impl.digester.elements.NavigationCase;
+import org.apache.myfaces.config.impl.digester.elements.NavigationRule;
+import org.apache.myfaces.config.impl.digester.elements.Property;
+import org.apache.myfaces.config.impl.digester.elements.RenderKit;
+import org.apache.myfaces.config.impl.digester.elements.Renderer;
+import org.apache.myfaces.config.impl.digester.elements.ResourceBundle;
+import org.apache.myfaces.shared_impl.util.ClassUtils;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -47,7 +64,8 @@ public class DigesterFacesConfigUnmarshallerImpl implements FacesConfigUnmarshal
         digester.setValidating(false);
         digester.setNamespaceAware(true);
         digester.setEntityResolver(new FacesConfigEntityResolver(externalContext));
-        digester.setUseContextClassLoader(true);
+        //digester.setUseContextClassLoader(true);
+        digester.setClassLoader(ClassUtils.getContextClassLoader());
 
         digester.addObjectCreate("faces-config", FacesConfig.class);
         digester.addObjectCreate("faces-config/application", Application.class);
