c718029cda36b948ea6ea27deeb96164f9f14b6f
Leonardo Uribe <lu4242@apache.org>
2010-02-03 19:14:41 +0000

    MYFACES-2530 ActionSourceRule does not deal with jsf 1.1 ActionSouce instances

--- impl/src/main/java/org/apache/myfaces/view/facelets/tag/jsf/ActionSourceRule.java
+++ impl/src/main/java/org/apache/myfaces/view/facelets/tag/jsf/ActionSourceRule.java
@@ -30,6 +30,7 @@ import javax.faces.view.facelets.MetadataTarget;
 import javax.faces.view.facelets.TagAttribute;
 
 import org.apache.myfaces.view.facelets.AbstractFaceletContext;
+import org.apache.myfaces.view.facelets.el.LegacyMethodBinding;
 
 /**
  * 
@@ -42,6 +43,21 @@ public final class ActionSourceRule extends MetaRule
 
     public final static Class<?>[] ACTION_LISTENER_SIG = new Class<?>[] { ActionEvent.class };
 
+    final static class ActionMapper extends Metadata {
+
+        private final TagAttribute attr;
+
+        public ActionMapper(TagAttribute attr) {
+            this.attr = attr;
+        }
+
+        public void applyMetadata(FaceletContext ctx, Object instance) {
+            ((ActionSource) instance).setAction(new LegacyMethodBinding(
+                    this.attr.getMethodExpression(ctx, String.class,
+                            ActionSourceRule.ACTION_SIG)));
+        }
+    }
+    
     final static class ActionMapper2 extends Metadata
     {
         private final TagAttribute _attr;
@@ -56,6 +72,23 @@ public final class ActionSourceRule extends MetaRule
             MethodExpression expr = _attr.getMethodExpression(ctx, String.class, ActionSourceRule.ACTION_SIG);
             ((ActionSource2) instance).setActionExpression(expr);
         }
+    }
+
+    final static class ActionListenerMapper extends Metadata {
+
+        private final TagAttribute attr;
+
+        public ActionListenerMapper(TagAttribute attr) {
+            this.attr = attr;
+        }
+
+        public void applyMetadata(FaceletContext ctx, Object instance) {
+            ((ActionSource) instance)
+                    .setActionListener(new LegacyMethodBinding(this.attr
+                            .getMethodExpression(ctx, null,
+                                    ActionSourceRule.ACTION_LISTENER_SIG)));
+        }
+
     }
 
     final static class ActionListenerMapper2 extends Metadata
@@ -94,12 +127,26 @@ public final class ActionSourceRule extends MetaRule
         {
             if ("action".equals(name))
             {
-                return new ActionMapper2(attribute);
+                if (meta.isTargetInstanceOf(ActionSource2.class))
+                {
+                    return new ActionMapper2(attribute);
+                }
+                else
+                {
+                    return new ActionMapper(attribute);
+                }
             }
 
             if ("actionListener".equals(name))
             {
-                return new ActionListenerMapper2(attribute);
+                if (meta.isTargetInstanceOf(ActionSource2.class))
+                {
+                    return new ActionListenerMapper2(attribute);
+                }
+                else
+                {
+                    return new ActionListenerMapper(attribute);
+                }
             }
         }
         
