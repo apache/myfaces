2d1f99fee1224fe7b1b51483acd9e624aa4f7843
Leonardo Uribe <lu4242@apache.org>
2010-02-05 23:40:56 +0000

    MYFACES-2520 UnsupportedOperationException when launching Trinidad 2 w/ MyFaces2 in Jetty

--- impl/src/main/java/org/apache/myfaces/application/_SystemEventServletRequest.java
+++ impl/src/main/java/org/apache/myfaces/application/_SystemEventServletRequest.java
@@ -18,13 +18,23 @@
  */
 package org.apache.myfaces.application;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpSession;
@@ -91,5 +101,297 @@ public class _SystemEventServletRequest extends HttpServletRequestWrapper{
     {
         return null;
     }
-    
+
+    @Override
+    public int getContentLength()
+    {
+        return -1;
+    }
+
+    @Override
+    public String getContentType()
+    {
+        return null;
+    }
+
+    @Override
+    public String getCharacterEncoding()
+    {
+        return null;
+    }
+
+    @Override
+    public String getHeader(String name)
+    {
+        return null;
+    }
+
+    @Override
+    public Enumeration getHeaderNames()
+    {
+        return Collections.enumeration(Collections.emptyList());
+    }
+
+    @Override
+    public Enumeration getHeaders(String name)
+    {
+        return Collections.enumeration(Collections.EMPTY_LIST);
+    }
+
+    @Override
+    public Cookie[] getCookies()
+    {
+        return new Cookie[0];
+    }
+
+    @Override
+    public String getAuthType()
+    {
+        return null;
+    }
+
+    @Override
+    public String getContextPath()
+    {
+        return null;
+    }
+
+    @Override
+    public long getDateHeader(String name)
+    {
+        return -1;
+    }
+
+    @Override
+    public int getIntHeader(String name)
+    {
+        return -1;
+    }
+
+    @Override
+    public String getMethod()
+    {
+        return null;
+    }
+
+    @Override
+    public String getPathTranslated()
+    {
+        return null;
+    }
+
+    @Override
+    public String getQueryString()
+    {
+        return null;
+    }
+
+    @Override
+    public String getRemoteUser()
+    {
+        return null;
+    }
+
+    @Override
+    public String getRequestedSessionId()
+    {
+        return null;
+    }
+
+    @Override
+    public String getRequestURI()
+    {
+        return null;
+    }
+
+    @Override
+    public StringBuffer getRequestURL()
+    {
+        return null;
+    }
+
+    @Override
+    public Principal getUserPrincipal()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromCookie()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromUrl()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromURL()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdValid()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isUserInRole(String role)
+    {
+        return false;
+    }
+
+    @Override
+    public Enumeration getAttributeNames()
+    {
+        return Collections.enumeration(Collections.EMPTY_LIST);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException
+    {
+        return null;
+    }
+
+    @Override
+    public String getLocalAddr()
+    {
+        return null;
+    }
+
+    @Override
+    public Locale getLocale()
+    {
+        return null;
+    }
+
+    @Override
+    public Enumeration getLocales()
+    {
+        return Collections.enumeration(Collections.emptyList());
+    }
+
+    @Override
+    public String getLocalName()
+    {
+        return null;
+    }
+
+    @Override
+    public int getLocalPort()
+    {
+        return -1;
+    }
+
+    @Override
+    public String getParameter(String name)
+    {
+        return null;
+    }
+
+    @Override
+    public Map getParameterMap()
+    {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public Enumeration getParameterNames()
+    {
+        return Collections.enumeration(Collections.emptyList());
+    }
+
+    @Override
+    public String[] getParameterValues(String name)
+    {
+        return new String[0];
+    }
+
+    @Override
+    public String getProtocol()
+    {
+        return null;
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException
+    {
+        return null;
+    }
+
+    @Override
+    public String getRealPath(String path)
+    {
+        return null;
+    }
+
+    @Override
+    public String getRemoteAddr()
+    {
+        return null;
+    }
+
+    @Override
+    public String getRemoteHost()
+    {
+        return null;
+    }
+
+    @Override
+    public int getRemotePort()
+    {
+        return -1;
+    }
+
+    @Override
+    public ServletRequest getRequest()
+    {
+        return null;
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String path)
+    {
+        return null;
+    }
+
+    @Override
+    public String getScheme()
+    {
+        return null;
+    }
+
+    @Override
+    public String getServerName()
+    {
+        return null;
+    }
+
+    @Override
+    public int getServerPort()
+    {
+        return -1;
+    }
+
+    @Override
+    public boolean isSecure()
+    {
+        return false;
+    }
+
+    @Override
+    public void setCharacterEncoding(String enc)
+            throws UnsupportedEncodingException
+    {
+    }
+
+    @Override
+    public void setRequest(ServletRequest request)
+    {
+    }
 }
