3c331a3bde8558754ee858312bf0b5ab30d0bb57
Werner Punz <werpu@apache.org>
2010-03-10 10:08:09 +0000

    https://issues.apache.org/jira/browse/MYFACES-2596
    quick fix for the issue but in the end this needs to be unified under share, our version has the better classloader resolution, the facelet version has primitives and array type resolution

--- impl/src/main/java/org/apache/myfaces/view/facelets/util/ReflectionUtil.java
+++ impl/src/main/java/org/apache/myfaces/view/facelets/util/ReflectionUtil.java
@@ -21,6 +21,7 @@ package org.apache.myfaces.view.facelets.util;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
+import org.apache.myfaces.shared_impl.util.ClassLoaderUtils;
 import org.apache.myfaces.shared_impl.util.ClassUtils;
 
 public class ReflectionUtil
@@ -55,12 +56,17 @@ public class ReflectionUtil
             if (name.endsWith("[]"))
             {
                 String nc = name.substring(0, name.length() - 2);
-                c = Class.forName(nc, false, ClassUtils.getContextClassLoader());
+                //we should route through our shared forName, due to plugins and due to bette4r classloader resolution
+                c  = ClassUtils.classForName(nc);
+                //old code left for double checking
+                //c = Class.forName(nc, false, ClassUtils.getContextClassLoader());
                 c = Array.newInstance(c, 0).getClass();
             }
             else
             {
-                c = Class.forName(name, false, ClassUtils.getContextClassLoader());
+                c  = ClassUtils.classForName(name);
+                //old code left for double checking
+                //c = Class.forName(name, false, ClassUtils.getContextClassLoader());
             }
         }
         
