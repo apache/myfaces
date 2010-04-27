dcc060d140449807fa672b041a44aeeee064eb94
Leonardo Uribe <lu4242@apache.org>
2010-04-27 01:18:21 +0000

    MYFACES-2679 Use a constant to identify FacesCompositeELResolver.Scope value

--- impl/src/main/java/org/apache/myfaces/el/unified/resolver/FacesCompositeELResolver.java
+++ impl/src/main/java/org/apache/myfaces/el/unified/resolver/FacesCompositeELResolver.java
@@ -53,6 +53,8 @@ public final class FacesCompositeELResolver extends org.apache.myfaces.el.Compos
     {
         Faces, JSP
     }
+    
+    public static final String SCOPE = "org.apache.myfaces.el.unified.resolver.FacesCompositeELResolver.Scope";
 
     public FacesCompositeELResolver(final Scope scope)
     {
@@ -188,11 +190,11 @@ public final class FacesCompositeELResolver extends org.apache.myfaces.el.Compos
 
     private void setScope(final Map<String, Object> requestMap)
     {
-        requestMap.put(Scope.class.getName(), _scope);
+        requestMap.put(SCOPE, _scope);
     }
 
     private static void unsetScope(final Map<String, Object> requestMap)
     {
-        requestMap.remove(Scope.class.getName());
+        requestMap.remove(SCOPE);
     }
 }
