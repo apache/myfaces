63ed3f400aebc8b9d728fda123c6f2959853d88f
Werner Punz <werpu@apache.org>
2010-05-14 20:11:03 +0000

    https://issues.apache.org/jira/browse/MYFACES-2716
    adding comments

--- api/src/main/javascript/META-INF/resources/myfaces/_impl/_util/_UnitTest.js
+++ api/src/main/javascript/META-INF/resources/myfaces/_impl/_util/_UnitTest.js
@@ -2,8 +2,12 @@
  * This class is for http based unit tests
  */
 myfaces._impl.core._Runtime.singletonExtendClass("myfaces._impl._util._UnitTest", Object, {
+
     /**
+     * Simple assert true
      *
+     * @param message the assertion message
+     * @param assertionOutcome the assertion outcome (true or false)
      */
     assertTrue: function(message, assertionOutcome) {
         var _Lang = myfaces._impl._util._Lang;
@@ -14,6 +18,13 @@ myfaces._impl.core._Runtime.singletonExtendClass("myfaces._impl._util._UnitTest"
         }
         _Lang.logInfo(message, "assertionOutcome:", assertionOutcome);
     },
+    
+    /**
+     * Simple assert false
+     *
+     * @param message the assertion message
+     * @param assertionOutcome the assertion outcome (true or false)
+     */
     assertFalse: function(message, assertionOutcome) {
         var _Lang = myfaces._impl._util._Lang;
 
