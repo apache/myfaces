1e74d7a7dc3406c14eba3a9fd99145e17eccdc53
Jakob Korherr <jakobk@apache.org>
2010-05-28 20:14:53 +0000

    simplified if clause

--- api/src/main/java/javax/faces/component/UISelectMany.java
+++ api/src/main/java/javax/faces/component/UISelectMany.java
@@ -379,10 +379,11 @@ public class UISelectMany extends UIInput
 
                 // selected value must match to one of the available options
                 // and if required is true it must not match an option with noSelectionOption set to true (since 2.0)
-                if (!(_SelectItemsUtil.matchValue(context, itemValue, items.iterator(), converter)
-                      && (!this.isRequired() 
-                          || (this.isRequired() 
-                              && !_SelectItemsUtil.isNoSelectionOption(context, itemValue, items.iterator(), converter)))))
+                if (!_SelectItemsUtil.matchValue(context, itemValue, items.iterator(), converter)
+                        || (
+                            this.isRequired()
+                            && _SelectItemsUtil.isNoSelectionOption(context, itemValue, items.iterator(), converter)
+                        ))
                 {    
                     _MessageUtils.addErrorMessage(context, this, INVALID_MESSAGE_ID,
                         new Object[] { _MessageUtils.getLabel(context, this) });
