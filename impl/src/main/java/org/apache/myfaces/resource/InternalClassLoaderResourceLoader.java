a5a1a31e979540280459079566d252a5691b6e6c
Leonardo Uribe <lu4242@apache.org>
2010-06-25 21:40:20 +0000

    MYFACES-2714 Include uncompressed jsf.js file and use it when development mode is used

--- impl/src/main/java/org/apache/myfaces/resource/InternalClassLoaderResourceLoader.java
+++ impl/src/main/java/org/apache/myfaces/resource/InternalClassLoaderResourceLoader.java
@@ -21,6 +21,10 @@ package org.apache.myfaces.resource;
 import java.io.InputStream;
 import java.net.URL;
 
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFWebConfigParam;
 import org.apache.myfaces.shared_impl.util.ClassUtils;
 
 /**
@@ -30,9 +34,22 @@ import org.apache.myfaces.shared_impl.util.ClassUtils;
 public class InternalClassLoaderResourceLoader extends ResourceLoader
 {
 
+    /**
+     * If this param is true and the project stage is development mode,
+     * the source javascript files will be loaded separately instead have
+     * all in just one file, to preserve line numbers and make javascript
+     * debugging of jsf.js more simple.
+     */
+    @JSFWebConfigParam(since="2.0.1",defaultValue="false",expectedValues="true,false")
+    public static final String USE_MULTIPLE_JS_FILES_FOR_JSF_UNCOMPRESSED_JS = "org.apache.myfaces.USE_MULTIPLE_JS_FILES_FOR_JSF_UNCOMPRESSED_JS";
+    
+    private final boolean _useMultipleJsFilesForJsfUncompressedJs;
+
     public InternalClassLoaderResourceLoader(String prefix)
     {
         super(prefix);
+        _useMultipleJsFilesForJsfUncompressedJs = _getBooleanParameter(FacesContext.getCurrentInstance().getExternalContext(),
+                USE_MULTIPLE_JS_FILES_FOR_JSF_UNCOMPRESSED_JS, null, false);
     }
 
     @Override
@@ -81,8 +98,15 @@ public class InternalClassLoaderResourceLoader extends ResourceLoader
                 org.apache.myfaces.shared_impl.renderkit.html.util.ResourceUtils.JAVAX_FACES_LIBRARY_NAME.equals(libraryName) &&
                 org.apache.myfaces.shared_impl.renderkit.html.util.ResourceUtils.JSF_JS_RESOURCE_NAME.equals(resourceName))
         {
-            return new AliasResourceMetaImpl(prefix, libraryName, libraryVersion,
+            if (_useMultipleJsFilesForJsfUncompressedJs)
+            {
+                return new AliasResourceMetaImpl(prefix, libraryName, libraryVersion,
                     resourceName, resourceVersion, org.apache.myfaces.shared_impl.renderkit.html.util.ResourceUtils.JSF_UNCOMPRESSED_JS_RESOURCE_NAME);
+            }
+            else
+            {
+                return new AliasResourceMetaImpl(prefix, libraryName, libraryVersion, resourceName, resourceVersion, "jsf-uncompressed-full.js");
+            }
         }
         else if (libraryName != null && libraryName.startsWith("org.apache.myfaces.core"))
         {
@@ -126,4 +150,41 @@ public class InternalClassLoaderResourceLoader extends ResourceLoader
         }
         return false;
     }
+
+    private String _getStringParameter(ExternalContext context, String name, String deprecatedName)
+    {
+        String param = context.getInitParameter(name);
+        
+        if ((param == null) && (deprecatedName != null))
+        {
+            param = context.getInitParameter (deprecatedName);
+        }
+        
+        if (param == null)
+        {
+            return null;
+        }
+
+        param = param.trim();
+        if (param.length() == 0)
+        {
+            return null;
+        }
+
+        return param;
+    }
+
+    private boolean _getBooleanParameter(ExternalContext context, String name, String deprecatedName, boolean defaultValue)
+    {
+        String param = _getStringParameter(context, name, deprecatedName);
+        if (param == null)
+        {
+            return defaultValue;
+        }
+        else
+        {
+            return Boolean.parseBoolean(param.toLowerCase());
+        }
+    }
+
 }
