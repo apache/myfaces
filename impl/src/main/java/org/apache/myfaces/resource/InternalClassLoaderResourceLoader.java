19601f76e32ecf1c8356dfe9f3020e9c5c606646
Jakob Korherr <jakobk@apache.org>
2011-02-23 11:51:06 +0000

    MYFACES-3051 Use multiple ClassLoaders to find resources (not only ContextClassLoader) (first draft)

--- impl/src/main/java/org/apache/myfaces/resource/InternalClassLoaderResourceLoader.java
+++ impl/src/main/java/org/apache/myfaces/resource/InternalClassLoaderResourceLoader.java
@@ -18,18 +18,17 @@
  */
 package org.apache.myfaces.resource;
 
-import java.io.InputStream;
-import java.net.URL;
-
-import javax.faces.context.FacesContext;
-
 import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFWebConfigParam;
 import org.apache.myfaces.shared_impl.resource.AliasResourceMetaImpl;
 import org.apache.myfaces.shared_impl.resource.ResourceLoader;
 import org.apache.myfaces.shared_impl.resource.ResourceMeta;
 import org.apache.myfaces.shared_impl.resource.ResourceMetaImpl;
-import org.apache.myfaces.shared_impl.util.ClassUtils;
+import org.apache.myfaces.shared_impl.util.MyFacesClassLoader;
 import org.apache.myfaces.shared_impl.util.WebConfigParamUtils;
+
+import javax.faces.context.FacesContext;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * A resource loader implementation which loads resources from the thread ClassLoader.
@@ -48,6 +47,7 @@ public class InternalClassLoaderResourceLoader extends ResourceLoader
     public static final String USE_MULTIPLE_JS_FILES_FOR_JSF_UNCOMPRESSED_JS = "org.apache.myfaces.USE_MULTIPLE_JS_FILES_FOR_JSF_UNCOMPRESSED_JS";
     
     private final boolean _useMultipleJsFilesForJsfUncompressedJs;
+    private MyFacesClassLoader classLoader;
 
     public InternalClassLoaderResourceLoader(String prefix)
     {
@@ -138,7 +138,11 @@ public class InternalClassLoaderResourceLoader extends ResourceLoader
      */
     protected ClassLoader getClassLoader()
     {
-        return ClassUtils.getContextClassLoader();
+        if (classLoader == null)
+        {
+            classLoader = new MyFacesClassLoader();
+        }
+        return classLoader;
     }
 
     @Override
