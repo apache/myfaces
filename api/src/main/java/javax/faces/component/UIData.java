b61314cedc647da9d338393f9a0ae082cc02bc88
Leonardo Uribe <lu4242@apache.org>
2011-06-02 03:00:36 +0000

    MYFACES-3165 UIData.broadcast does not push composite component on EL stack

--- api/src/main/java/javax/faces/component/UIData.java
+++ api/src/main/java/javax/faces/component/UIData.java
@@ -768,8 +768,13 @@ public class UIData extends UIComponentBase implements NamingContainer, UniqueId
             int eventRowIndex = ((FacesEventWrapper) event).getRowIndex();
             final int currentRowIndex = getRowIndex();
             UIComponent source = originalEvent.getComponent();
-            
+            UIComponent compositeParent = UIComponent.getCompositeComponentParent(source);
+
             setRowIndex(eventRowIndex);
+            if (compositeParent != null)
+            {
+                pushComponentToEL(getFacesContext(), compositeParent);
+            }
             pushComponentToEL(getFacesContext(), source);
             try
             {
@@ -778,6 +783,10 @@ public class UIData extends UIComponentBase implements NamingContainer, UniqueId
             finally
             {
                 popComponentFromEL(getFacesContext());
+                if (compositeParent != null)
+                {
+                    popComponentFromEL(getFacesContext());
+                }
                 setRowIndex(currentRowIndex);
             }
         }
