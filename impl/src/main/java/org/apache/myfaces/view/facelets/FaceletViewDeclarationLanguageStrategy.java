a6b261244c9d6b6061006fff19f3becaf0aa7e7c
Leonardo Uribe <lu4242@apache.org>
2011-08-14 18:17:22 +0000

    MYFACES-3280 facelets.VIEWMAPPINGS should be renamed as facelets.VIEW_MAPPINGS

--- impl/src/main/java/org/apache/myfaces/view/facelets/FaceletViewDeclarationLanguageStrategy.java
+++ impl/src/main/java/org/apache/myfaces/view/facelets/FaceletViewDeclarationLanguageStrategy.java
@@ -93,6 +93,10 @@ public class FaceletViewDeclarationLanguageStrategy implements ViewDeclarationLa
         assert context != null;
 
         String mappings = context.getInitParameter(ViewHandler.FACELETS_VIEW_MAPPINGS_PARAM_NAME);
+        if(mappings == null)    //consider alias facelets.VIEW_MAPPINGS
+        {
+            mappings = context.getInitParameter("facelets.VIEW_MAPPINGS");
+        }
         if (mappings == null)
         {
             return null;
