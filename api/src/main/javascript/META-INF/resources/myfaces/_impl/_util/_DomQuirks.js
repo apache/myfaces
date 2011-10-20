9f0a93880045e554a010f89d5a01ad76e29663fa
Werner Punz <werpu@apache.org>
2011-10-20 09:27:33 +0000

    https://issues.apache.org/jira/browse/MYFACES-3361
    Fortifying the extension packages so that they
    do not cause errors if the core is not loaded.
    The idea simply is to peform a check for a core api
    before the class is initialized, this can be done
    with a simple && before calling the singleton and 
    class initializer.

--- api/src/main/javascript/META-INF/resources/myfaces/_impl/_util/_DomQuirks.js
+++ api/src/main/javascript/META-INF/resources/myfaces/_impl/_util/_DomQuirks.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-if(!document.querySelectorAll) {
+if(!document.querySelectorAll && _MF_SINGLTN) {
 /***
  Dom.js version for non html5 browsers
  */
--- api/src/main/javascript/META-INF/resources/myfaces/_impl/core/_StartImpl.js
+++ api/src/main/javascript/META-INF/resources/myfaces/_impl/core/_StartImpl.js
@@ -15,19 +15,22 @@
  */
 /*we cannot privatize with a global function hence we store the values away for the init part*/
 (function() {
-    //some mobile browsers do not have a window object
     var target = window || document.body;
-    var _RT = myfaces._impl.core._Runtime;
-    var impl = "myfaces._impl.";
-    var params = {_MF_CLS: _RT.extendClass,
-        _MF_SINGLTN: _RT.singletonExtendClass,
-        _PFX_UTIL: impl+"_util.",
-        _PFX_CORE:impl+"core.",
-        _PFX_XHR: impl+"xhrCore.",
-        _PFX_I18N: impl+"i18n."};
+    if (target.myfaces && target.myfaces._impl) {
+        //some mobile browsers do not have a window object
 
-    for (var key in params) {
-        _RT[key] = target[key];
-        target[key] = params[key];
+        var _RT = myfaces._impl.core._Runtime;
+        var impl = "myfaces._impl.";
+        var params = {_MF_CLS: _RT.extendClass,
+            _MF_SINGLTN: _RT.singletonExtendClass,
+            _PFX_UTIL: impl + "_util.",
+            _PFX_CORE:impl + "core.",
+            _PFX_XHR: impl + "xhrCore.",
+            _PFX_I18N: impl + "i18n."};
+
+        for (var key in params) {
+            _RT[key] = target[key];
+            target[key] = params[key];
+        }
     }
 })();
