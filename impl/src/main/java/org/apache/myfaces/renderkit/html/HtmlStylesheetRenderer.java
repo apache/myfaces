db7110b9aff8c582f06926090ed8e51346e966ce
Leonardo Uribe <lu4242@apache.org>
2013-06-18 17:07:53 +0000

    MYFACES-3738 Add media attribute to h:outputStylesheet

--- impl/src/main/java/org/apache/myfaces/renderkit/html/HtmlStylesheetRenderer.java
+++ impl/src/main/java/org/apache/myfaces/renderkit/html/HtmlStylesheetRenderer.java
@@ -236,7 +236,11 @@ public class HtmlStylesheetRenderer extends Renderer implements
             writer.startElement(HTML.LINK_ELEM, component);
             writer.writeAttribute(HTML.REL_ATTR, HTML.STYLESHEET_VALUE,null );
             String media = (String) component.getAttributes().get("media");
-            writer.writeAttribute("media", media == null ? "screen" : media ,null );
+            //writer.writeAttribute("media", media == null ? "screen" : media ,null );
+            if (media != null)
+            {
+                writer.writeAttribute("media", media ,null );
+            }
             writer.writeAttribute(HTML.TYPE_ATTR, 
                     (resource.getContentType() == null ? HTML.STYLE_TYPE_TEXT_CSS
                             : resource.getContentType()) , null);
