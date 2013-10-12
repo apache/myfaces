c4bfaacea0b494dc0e4734f7b5b8a69b01183be4
Gerhard Petracek <gpetracek@apache.org>
2013-10-12 20:18:34 +0000

    MYFACES-3797 cleanup

--- impl/src/main/java/org/apache/myfaces/application/cdi/AbstractBeanStorage.java
+++ impl/src/main/java/org/apache/myfaces/application/cdi/AbstractBeanStorage.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class AbstractBeanStorage
+public abstract class AbstractBeanStorage
 {
     private static final Logger LOG = Logger.getLogger(AbstractBeanStorage.class.getName());
 
