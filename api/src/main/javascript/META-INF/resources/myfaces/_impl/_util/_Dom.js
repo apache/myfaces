348200e1993ca43bcef79868a61f61ba894c098d
Werner Punz <werpu@apache.org>
2014-06-23 09:40:09 +0000

    https://issues.apache.org/jira/browse/MYFACES-3902

--- api/src/main/javascript/META-INF/resources/myfaces/_impl/_util/_Dom.js
+++ api/src/main/javascript/META-INF/resources/myfaces/_impl/_util/_Dom.js
@@ -185,16 +185,16 @@ _MF_SINGLTN(_PFX_UTIL + "_Dom", Object, /** @lends myfaces._impl._util._Dom.prot
             }
         } catch (e) {
             if(window.console && window.console.error) {
-               //not sure if we
-               //should use our standard
-               //error mechanisms here
-               //because in the head appendix
-               //method only a console
-               //error would be raised as well
-               console.error(e.message||e.description);
+                //not sure if we
+                //should use our standard
+                //error mechanisms here
+                //because in the head appendix
+                //method only a console
+                //error would be raised as well
+                console.error(e.message || e.description || e);
             } else {
-                if(jsf.ajax.getProjectStage() === "Development") {
-                    alert("Error in evaluated javascript:"+ (e.message||e.description));
+                if(jsf.getProjectStage() === "Development") {
+                    alert("Error in evaluated javascript:"+ (e.message || e.description || e));
                 }
             }
         } finally {
