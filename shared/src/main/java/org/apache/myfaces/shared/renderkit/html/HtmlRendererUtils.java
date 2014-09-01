bcd27fbfd7b96e3dbe5f4bb749cd6394b4ff68f0
Leonardo Uribe <lu4242@apache.org>
2014-09-01 20:35:38 +0000

    MYFACES-3919 javax.faces.SEPARATOR_CHAR Applied Incorrectly to commandLink Hidden Field (Thanks to Bill Lucy for provide this patch)

--- shared/src/main/java/org/apache/myfaces/shared/renderkit/html/HtmlRendererUtils.java
+++ shared/src/main/java/org/apache/myfaces/shared/renderkit/html/HtmlRendererUtils.java
@@ -1581,8 +1581,8 @@ public final class HtmlRendererUtils
         {
             return HIDDEN_COMMANDLINK_FIELD_NAME_TRINIDAD;
         }
-        return formInfo.getFormName() + UINamingContainer.getSeparatorChar(FacesContext
-                        .getCurrentInstance()) + HIDDEN_COMMANDLINK_FIELD_NAME;
+        return formInfo.getFormName() + ':' 
+                + HIDDEN_COMMANDLINK_FIELD_NAME;
     }
     
     public static String getHiddenCommandLinkFieldName(
@@ -1592,7 +1592,7 @@ public final class HtmlRendererUtils
         {
             return HIDDEN_COMMANDLINK_FIELD_NAME_TRINIDAD;
         }
-        return formInfo.getFormName() + UINamingContainer.getSeparatorChar(facesContext)
+        return formInfo.getFormName() + ':'
                 + HIDDEN_COMMANDLINK_FIELD_NAME;
     }
 
@@ -1626,7 +1626,7 @@ public final class HtmlRendererUtils
     public static String getHiddenCommandLinkFieldNameMyfacesOld(
             FormInfo formInfo)
     {
-        return formInfo.getFormName() + UINamingContainer.getSeparatorChar(FacesContext.getCurrentInstance())
+        return formInfo.getFormName() + ':'
                 + HIDDEN_COMMANDLINK_FIELD_NAME_MYFACES_OLD;
     }
 
