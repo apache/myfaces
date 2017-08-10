e44f2dfb21ea2363a96f0bcbf1faa54a8fba408b
Thomas Andraschko <tandraschko@apache.org>
2017-08-10 18:39:12 +0000

    (MYFACES-3163) [perf] review working with SelectItem(s)

--- shared/src/main/java/org/apache/myfaces/shared/renderkit/html/util/SelectItemsUtils.java
+++ shared/src/main/java/org/apache/myfaces/shared/renderkit/html/util/SelectItemsUtils.java
@@ -20,7 +20,6 @@ package org.apache.myfaces.shared.renderkit.html.util;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import javax.faces.component.UIComponent;
