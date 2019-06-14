49ae2503e1daaa80ae026666b412f1a1bf42583d
Tobias Rettstadt <glei.tobias.rettstadt@nordlb.de>
2019-06-14 14:49:07 +0200

    MYFACES-4293: handle BigInteger in NumberConverter
    (cherry picked from commit d6b197f75e3369d54eb4e939b4b56f725ffe3a25)
    
    Change-Id: I1934cc3a71833d965a39fc5df27df6aad6103f81

--- api/src/main/java/javax/faces/convert/NumberConverter.java
+++ api/src/main/java/javax/faces/convert/NumberConverter.java
@@ -19,6 +19,7 @@
 package javax.faces.convert;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
@@ -114,10 +115,12 @@ public class NumberConverter
                 // in those cases it is expected to return Double). See MYFACES-1890 and TRINIDAD-1124
                 // for details
                 ValueExpression valueBinding = uiComponent.getValueExpression("value");
+                Class<?> destType = null;
                 if (valueBinding != null)
                 {
-                    Class<?> destType = valueBinding.getType(facesContext.getELContext());
-                    if (destType != null && BigDecimal.class.isAssignableFrom(destType))
+                    destType = valueBinding.getType(facesContext.getELContext());
+                    if (destType != null
+                        && (BigDecimal.class.isAssignableFrom(destType) || BigInteger.class.isAssignableFrom(destType)))
                     {
                         df.setParseBigDecimal(true);
                     }
@@ -137,7 +140,7 @@ public class NumberConverter
                 
                 try
                 {
-                    return format.parse(value);
+                    return parse(value, format, destType);
                 }
                 catch (ParseException e)
                 {
@@ -148,7 +151,7 @@ public class NumberConverter
                   }
                   try
                   {
-                    return format.parse(value);
+                      return parse(value, format, destType);
                   }
                   catch (ParseException pe)
                   {
@@ -185,6 +188,19 @@ public class NumberConverter
             }
         }
         return null;
+    }
+
+    private Object parse(String value, NumberFormat format, Class<?> destType)
+        throws ParseException
+    {
+        if (destType == BigInteger.class)
+        {
+            return ((BigDecimal) format.parse(value)).toBigInteger();
+        }
+        else
+        {
+            return format.parse(value);
+        }
     }
 
     public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object value)
