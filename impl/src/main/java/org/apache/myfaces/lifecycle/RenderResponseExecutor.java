ef681d17bdd3089857216e00971294539948ba04
Bill Lucy <wtlucy@gmail.com>
2019-11-12 16:20:01 -0500

    MYFACES-4309: create session in render response if needed If a given view is not transient and server side state saving is used, we should always create a session since some scopes need one.  A new parm ALWAYS_FORCE_SESSION_CREATION (default=true in 2.2) is added to enable that behavior even if a view is transient or client side state saving is enabled.

--- impl/src/main/java/org/apache/myfaces/lifecycle/RenderResponseExecutor.java
+++ impl/src/main/java/org/apache/myfaces/lifecycle/RenderResponseExecutor.java
@@ -34,6 +34,8 @@ import javax.faces.event.PhaseId;
 import javax.faces.event.PreRenderViewEvent;
 import javax.faces.view.ViewDeclarationLanguage;
 
+import org.apache.myfaces.shared.config.MyfacesConfig;
+
 /**
  * Implements the render response phase (JSF Spec 2.2.6)
  * 
@@ -44,6 +46,7 @@ class RenderResponseExecutor extends PhaseExecutor
 {
     
     private static final Logger log = Logger.getLogger(RenderResponseExecutor.class.getName());
+    private MyfacesConfig _myfacesConfig;
     
     public boolean execute(FacesContext facesContext)
     {
@@ -61,7 +64,9 @@ class RenderResponseExecutor extends PhaseExecutor
         {
             throw new ViewNotFoundException("A view is required to execute "+facesContext.getCurrentPhaseId());
         }
-        
+
+        forceSessionCreation(facesContext);
+
         try
         {
             // do-while, because the view might change in PreRenderViewEvent-listeners
@@ -153,5 +158,36 @@ class RenderResponseExecutor extends PhaseExecutor
     public PhaseId getPhase()
     {
         return PhaseId.RENDER_RESPONSE;
+    }
+
+    /**
+     * Create a session if the ALWAYS_FORCE_SESSION_CREATION param is set to true, or if the
+     * current view is not transient and server side state saving is in use.
+     * 
+     * Note: if the current view is transient or client side state saving is in use, it is 
+     * not technically correct to create a session here, since a session should not be
+     * required for those cases and creating one will cause undesirable memory usage.  
+     * However, if we do not create a session before rendering begins and view or session
+     * scope beans are created later on, then the response might be committed before those 
+     * scopes have a chance to create a session and so the session cookie will not be set.
+     * See MYFACES-4309
+     * 
+     * @param FacesContext
+     */
+    private void forceSessionCreation(FacesContext context) 
+    {
+        if (context.getExternalContext().getSession(false) == null) 
+        {
+            if (_myfacesConfig == null) 
+            {
+                _myfacesConfig = MyfacesConfig.getCurrentInstance(context.getExternalContext());
+            }
+            if (_myfacesConfig.isAlwaysForceSessionCreation() 
+                    || (!context.getViewRoot().isTransient() 
+                    && !context.getApplication().getStateManager().isSavingStateInClient(context))) 
+            {
+                context.getExternalContext().getSession(true);
+            }
+        }
     }
 }
