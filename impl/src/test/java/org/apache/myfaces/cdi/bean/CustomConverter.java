181d0e3c6a5f4cbbc4578461dbe9190a271c601b
Volodymyr Siedlecki <siedlecki@volodymyrs-mbp.raleigh.ibm.com>
2020-01-09 13:07:50 -0500

    add unit tests for generic converter & validator

---
+++ impl/src/test/java/org/apache/myfaces/cdi/bean/CustomConverter.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.cdi.bean;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(value="customConverter", managed = true)
+public class CustomConverter implements Converter<TestBean> {
+
+   @Override
+   public TestBean getAsObject(FacesContext facesContext, UIComponent component, String value) {
+      return new TestBean();
+   }
+
+   @Override
+   public String getAsString(FacesContext facesContext, UIComponent component, TestBean value) {
+      return value.getText();
+   }
+}
