5bc3a87da5cb9350b2886d6c6c9e8603ea8ca536
Volodymyr Siedlecki <volosied@gmail.com>
2021-10-25 16:04:41 -0400

    change terms

--- impl/src/main/java/org/apache/myfaces/application/ApplicationImpl.java
+++ impl/src/main/java/org/apache/myfaces/application/ApplicationImpl.java
@@ -1930,7 +1930,7 @@ public class ApplicationImpl extends Application
         if(context.isProjectStage(ProjectStage.Production) && !isCachedList && dependencyList != null)
         {
             // Note at this point dependencyList cannot be null, but just let this
-            // as a sanity check.
+            // as a verification check.
             _classToResourceDependencyMap.put(inspectedClass, dependencyList);
         }
         
@@ -2375,7 +2375,7 @@ public class ApplicationImpl extends Application
         if(isProduction && !isCachedList && listenerForList != null) 
         {
             // Note at this point listenerForList cannot be null, but just let listenerForList != null
-            // as a sanity check.
+            // as a verification check.
             _classToListenerForMap.put(inspectedClass, listenerForList);
         }
     }
@@ -2522,7 +2522,7 @@ public class ApplicationImpl extends Application
         if(isProduction && !isCachedList && dependencyList != null)   
         {
             // Note at this point listenerForList cannot be null, but just let dependencyList != null
-            // as a sanity check.
+            // as a verification check.
             _classToResourceDependencyMap.put(inspectedClass, dependencyList);
         }
         
