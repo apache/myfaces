f061bd7362b30063b90afc73f88f8b4d1bd0080e
Paulo Cristovão de Araújo Silva Filho <pcristof@gmail.com>
2022-10-30 10:27:02 -0300

    [localized-composite] Fixing localized properties resource loading

--- api/src/main/java/javax/faces/component/UIComponent.java
+++ api/src/main/java/javax/faces/component/UIComponent.java
@@ -679,8 +679,7 @@ public abstract class UIComponent
                     // Call ResourceHandler.createResource(java.lang.String,java.lang.String), passing the derived
                     // resourceName and
                     // libraryName.
-                    Resource bundleResource = context.getApplication().getResourceHandler()
-                            .createResource(resourceName, componentResource.getLibraryName());
+                    Resource bundleResource = getLocalizedCompositeResource(resourceName, componentResource.getLibraryName(), context);
 
                     if (bundleResource != null)
                     {
@@ -1831,4 +1830,42 @@ public abstract class UIComponent
             }
         }
     }
+    
+    private Resource getLocalizedCompositeResource(String resourceName, String libraryName, FacesContext context) 
+    {
+    	List<String> localizedPaths = getLocalizedPropertiesPaths(resourceName, context);
+    	Resource resource = null;
+    	
+    	for (String path_: localizedPaths) {
+    		resource = context.getApplication().getResourceHandler().createResource(path_, libraryName);
+    		if (resource != null)
+    		{
+    			break;
+    		}
+    	}
+    	
+    	return resource;
+    }
+    
+    private List<String> getLocalizedPropertiesPaths(String path, FacesContext ctx) 
+    {
+    	Locale loc = (ctx != null && ctx.getViewRoot() != null) ? ctx.getViewRoot().getLocale() : null;
+    	if (!path.endsWith(".properties") || loc == null) {
+    		return Collections.singletonList(path);
+    	}
+    	List<String> list = new ArrayList<>();
+    	String base = path.substring(0, path.lastIndexOf(".properties"));
+    	if (!loc.getVariant().isEmpty()) {
+    		list.add(String.format("%s_%s_%s_%s.properties", base, loc.getLanguage(), loc.getCountry(), loc.getVariant()));
+    	}
+    	if (!loc.getCountry().isEmpty()) {
+    		list.add(String.format("%s_%s_%s.properties", base, loc.getLanguage(), loc.getCountry()));
+    	}
+    	if (!loc.getLanguage().isEmpty()) {
+    		list.add(String.format("%s_%s.properties", base, loc.getLanguage()));
+    	}
+    	list.add(path);
+    	return list;
+    }
+    	
 }
