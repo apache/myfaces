afa4e0b86e8b282fa65a2810774810a7a272e9c6
Werner Punz <werner.punz@gmail.com>
2022-12-05 16:19:02 +0100

    https://issues.apache.org/jira/browse/MYFACES-4466: fix for a small regression introduce in the id generation code for the viewstate

--- api/src/client/typescript/faces/impl/util/HiddenInputBuilder.ts
+++ api/src/client/typescript/faces/impl/util/HiddenInputBuilder.ts
@@ -55,8 +55,10 @@ export class HiddenInputBuilder {
         let cnt = existingStates.stream.map(state => {
             let ident: string = state.id.orElse("-1").value;
             ident = ident.substring(ident.lastIndexOf(SEP)+1);
-            return parseInt(ident) || -1;
-        }).reduce((item1, item2) => Math.max(item1, item2), -1).value;
+            return parseInt(ident);
+        })
+            .filter(item => !isNaN(item))
+            .reduce((item1, item2) => Math.max(item1, item2), -1).value;
         //the maximum  new ident is the current max + 1
         cnt++;
 
