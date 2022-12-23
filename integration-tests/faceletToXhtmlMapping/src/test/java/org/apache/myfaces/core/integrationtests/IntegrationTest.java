50186684e6379d7387abb32db4b7b2de214e596d
Bernd Bohmann <bommel@apache.org>
2022-12-23 13:30:33 +0100

    fix: checkstyle and rat errors from jenkins build, change version back to snapshot, update tomcat version, workaround for serializer.jar' error message from tomcat jar scanner

--- integration-tests/faceletToXhtmlMapping/src/test/java/org/apache/myfaces/core/integrationtests/IntegrationTest.java
+++ integration-tests/faceletToXhtmlMapping/src/test/java/org/apache/myfaces/core/integrationtests/IntegrationTest.java
@@ -42,6 +42,24 @@ public class IntegrationTest
     @Deployment(testable = false)
     public static WebArchive createDeployment()
     {
+        // Fix for 'Failed to scan serializer.jar' error message
+        String key = "tomcat.util.scan.StandardJarScanFilter.jarsToSkip";
+        String value = "bootstrap.jar,commons-daemon.jar,tomcat-juli.jar,annotations-api.jar,el-api.jar,jsp-api.jar," +
+                "servlet-api.jar,websocket-api.jar,jaspic-api.jar,catalina.jar,catalina-ant.jar,catalina-ha.jar," +
+                "catalina-storeconfig.jar,catalina-tribes.jar,jasper.jar,jasper-el.jar,ecj-*.jar,tomcat-api.jar," +
+                "tomcat-util.jar,tomcat-util-scan.jar,tomcat-coyote.jar,tomcat-dbcp.jar,tomcat-jni.jar," +
+                "tomcat-websocket.jar,tomcat-i18n-en.jar,tomcat-i18n-es.jar,tomcat-i18n-fr.jar,tomcat-i18n-ja.jar," +
+                "tomcat-juli-adapters.jar,catalina-jmx-remote.jar,catalina-ws.jar,tomcat-jdbc.jar,tools.jar," +
+                "commons-beanutils*.jar,commons-codec*.jar,commons-collections*.jar,commons-dbcp*.jar," +
+                "commons-digester*.jar,commons-fileupload*.jar,commons-httpclient*.jar,commons-io*.jar," +
+                "commons-lang*.jar,commons-logging*.jar,commons-math*.jar,commons-pool*.jar,jstl.jar," +
+                "taglibs-standard-spec-*.jar,geronimo-spec-jaxrpc*.jar,wsdl4j*.jar,ant.jar,ant-junit*.jar," +
+                "aspectj*.jar,jmx.jar,h2*.jar,hibernate*.jar,httpclient*.jar,jmx-tools.jar,jta*.jar,log4j*.jar," +
+                "mail*.jar,slf4j*.jar,xercesImpl.jar,xmlParserAPIs.jar,xml-apis.jar,junit.jar,junit-*.jar," +
+                "ant-launcher.jar,cobertura-*.jar,asm-*.jar,dom4j-*.jar,icu4j-*.jar,jaxen-*.jar,jdom-*.jar," +
+                "jetty-*.jar,oro-*.jar,servlet-api-*.jar,tagsoup-*.jar,xmlParserAPIs-*.jar,xom-*.jar,serializer.jar";
+        System.setProperty(key, value);
+
         return ShrinkWrap.create(ZipImporter.class, "faceletToXhtmlMapping.war")
                 .importFrom(new File("target/faceletToXhtmlMapping.war"))
                 .as(WebArchive.class);
