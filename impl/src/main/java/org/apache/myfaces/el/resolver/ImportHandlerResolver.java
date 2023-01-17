c4206c91e2121ea4a58c0add5bde6084dea112d3
Thomas Andraschko <tandraschko@apache.org>
2023-01-17 17:50:49 +0100

    MYFACES-4549

--- impl/src/main/java/org/apache/myfaces/el/resolver/ImportHandlerResolver.java
+++ impl/src/main/java/org/apache/myfaces/el/resolver/ImportHandlerResolver.java
@@ -18,60 +18,17 @@
  */
 package org.apache.myfaces.el.resolver;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
+import jakarta.el.ELClass;
 import jakarta.el.ELContext;
 import jakarta.el.ELException;
+import jakarta.el.ImportHandler;
 import jakarta.el.PropertyNotFoundException;
-
-import org.apache.myfaces.util.lang.ClassUtils;
 
 /**
  * See Faces 2.2 section 5.6.2.8
  */
 public class ImportHandlerResolver extends ScopedAttributeResolver
 {
-    private static final Class EL_CLASS;
-    private static final Constructor EL_CLASS_CONSTRUCTOR;
-    private static final Method GET_IMPORT_HANDLER_METHOD;
-    private static final Method IMPORT_HANDLER_RESOLVE_CLASS_METHOD;
-    
-    static
-    {
-        Class elClass = null;
-        Class importHandlerClass = null;
-        Constructor elClassConstructor = null;
-        Method getImportHandlerMethod = null;
-        Method importHandlerResolveClassMethod = null;
-        try
-        {
-            // These classes will only be available with EL 3+
-            elClass = ClassUtils.classForName("jakarta.el.ELClass");
-            importHandlerClass = ClassUtils.classForName("jakarta.el.ImportHandler");
-            getImportHandlerMethod = ELContext.class.getMethod("getImportHandler");
-            if (elClass != null && importHandlerClass != null) 
-            {
-                importHandlerResolveClassMethod = 
-                    importHandlerClass.getDeclaredMethod("resolveClass", new Class[] {String.class});
-                elClassConstructor = elClass.getConstructor(Class.class);
-            }
-        }
-        catch (SecurityException | ClassNotFoundException | NoSuchMethodException ex)
-        {
-            //No op
-        }
-
-        EL_CLASS = elClass;
-        GET_IMPORT_HANDLER_METHOD = getImportHandlerMethod;
-        IMPORT_HANDLER_RESOLVE_CLASS_METHOD = importHandlerResolveClassMethod;
-        EL_CLASS_CONSTRUCTOR = elClassConstructor;
-    }
-    
-    /**
-     * Creates a new instance of ImportHandlerResolver
-     */
     public ImportHandlerResolver()
     {
     }
@@ -80,37 +37,19 @@ public class ImportHandlerResolver extends ScopedAttributeResolver
      * Handle the EL case for the name of an import class
      */
     @Override
-    public Object getValue(final ELContext context, final Object base, final Object property)
+    public Object getValue(ELContext context, Object base, Object property)
         throws NullPointerException, PropertyNotFoundException, ELException
     {
-        if (EL_CLASS == null
-                || EL_CLASS_CONSTRUCTOR == null 
-                || GET_IMPORT_HANDLER_METHOD == null
-                || IMPORT_HANDLER_RESOLVE_CLASS_METHOD == null)
+        ImportHandler importHandler = context.getImportHandler();
+        if (importHandler != null) 
         {
-            return null;
-        }
-
-        try 
-        {
-            // In an EL 3+ environment, the ELContext will have a getImportHandler() method
-            Object importHandler = GET_IMPORT_HANDLER_METHOD.invoke(context);
-            if (importHandler != null) 
+            Class<?> clazz = importHandler.resolveClass(property.toString());
+            if (clazz != null) 
             {
-                Class<?> clazz = (Class<?>) IMPORT_HANDLER_RESOLVE_CLASS_METHOD.invoke(
-                        importHandler, property.toString());
-                if (clazz != null) 
-                {
-                    context.setPropertyResolved(true);
-                    return EL_CLASS_CONSTRUCTOR.newInstance(clazz);
-                }
+                context.setPropertyResolved(true);
+                return new ELClass(clazz);
             }
-        } 
-        catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException
-                | InstantiationException ex) 
-        {
-            //No op
-        }            
+        }
 
         return null;
     }
