38848ef6bbf223082af344edbc05adf73016892b
Melloware <mellowaredev@gmail.com>
2023-04-03 11:27:37 -0400

    MYFACES-4598: 2.3next Quarkus do not register ClientProxy (#560)

--- extensions/quarkus/deployment/src/main/java/org/apache/myfaces/core/extensions/quarkus/deployment/MyFacesProcessor.java
+++ extensions/quarkus/deployment/src/main/java/org/apache/myfaces/core/extensions/quarkus/deployment/MyFacesProcessor.java
@@ -787,8 +787,6 @@ class MyFacesProcessor
         {
             // register type
             reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, type.name().toString()));
-            // and try to register the ClientProxy
-            reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, type.name().toString() + "_ClientProxy"));
         }
         
 
@@ -800,8 +798,6 @@ class MyFacesProcessor
         {
             // register type
             reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, typeName));
-            // and try to register the ClientProxy
-            reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, typeName + "_ClientProxy"));
         }
     }
     
