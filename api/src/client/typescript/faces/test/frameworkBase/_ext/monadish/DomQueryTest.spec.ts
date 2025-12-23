5ff9148db6ead1988f55e105a6be73561f45415d
werpu <werner.punz@gmail.com>
2025-12-23 22:54:42 +0100

    https://issues.apache.org/jira/browse/MYFACES-4736:fix for code under wrong license... moving up dependencies via audit

--- api/src/client/typescript/faces/test/frameworkBase/_ext/monadish/DomQueryTest.spec.ts
+++ api/src/client/typescript/faces/test/frameworkBase/_ext/monadish/DomQueryTest.spec.ts
@@ -17,7 +17,7 @@
 import {expect} from 'chai';
 import {describe, it} from 'mocha';
 import {from} from "rxjs";
-import {ArrayCollector, Config, DomQuery, DomQueryCollector, Lang, LazyStream, Stream} from "mona-dish/dist/js/umd";
+import {ArrayCollector, Config, DomQuery, DomQueryCollector, Lang, LazyStream, Stream} from "mona-dish";
 import {tobagoSheetWithHeader} from "./markups/tobago-with-header";
 import {tobagoSheetWithoutHeader} from "./markups/tobago-without-header";
 
