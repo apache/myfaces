a1b72cb061725ad0cb8f344fd3da6349bfedbf64
tandraschko <tandraschko@apache.org>
2026-07-07 23:30:19 +0200

    increase buffer for tests

--- test/src/main/java/org/apache/myfaces/test/mock/MockHttpServletResponse.java
+++ test/src/main/java/org/apache/myfaces/test/mock/MockHttpServletResponse.java
@@ -344,7 +344,7 @@ public class MockHttpServletResponse implements HttpServletResponse
                 throw new IllegalStateException(
                         "Cannot call getWriter() after getOutputStream() was called");
             }
-            writer = new MockPrintWriter(new CharArrayWriter());
+            writer = new MockPrintWriter(new CharArrayWriter(1024));
         }
         return writer;
 
